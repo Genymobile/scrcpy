2fdc368c419a2cd462eb0562cebb3f1bc05fa022
Romain Vimont <rvimont@genymobile.com>
2018-02-09 09:56:02 +0100

    Do not try to decode video when EOF is reached
    When the video stream socket is closed and read_packey() returns -1,
    av_read_frame() still returns 0.
    
    To detect EOF, check the flag eof_reached in the AVIOContext.
    
    This avoids garbage errors on closing.

--- app/src/decoder.c
+++ app/src/decoder.c
@@ -92,7 +92,7 @@ static int run_decoder(void *data) {
     packet.data = NULL;
     packet.size = 0;
 
-    while (!av_read_frame(format_ctx, &packet)) {
+    while (!av_read_frame(format_ctx, &packet) && !avio_ctx->eof_reached) {
 // the new decoding/encoding API has been introduced by:
 // <http://git.videolan.org/?p=ffmpeg.git;a=commitdiff;h=7fc329e2dd6226dfecaa4a1d7adf353bf2773726>
 #if LIBAVCODEC_VERSION_INT < AV_VERSION_INT(57, 37, 0)
