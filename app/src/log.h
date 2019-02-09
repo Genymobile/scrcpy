c11905b8606f257b9c1e421ef755c7240fcb036f
Romain Vimont <rom@rom1v.com>
2019-02-09 12:05:39 +0100

    Add log verbose macro
    This was the only log priority missing.

--- app/src/log.h
+++ app/src/log.h
@@ -3,6 +3,7 @@
 
 #include <SDL2/SDL_log.h>
 
+#define LOGV(...) SDL_LogVerbose(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 #define LOGD(...) SDL_LogDebug(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 #define LOGI(...) SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
 #define LOGW(...) SDL_LogWarn(SDL_LOG_CATEGORY_APPLICATION, __VA_ARGS__)
