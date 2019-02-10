6c40dbd27dbc5c3e9b6904eda4c9283f54374007
Romain Vimont <rom@rom1v.com>
2019-02-10 13:16:55 +0100

    Regroup Windows-ifdefs in command.h

--- app/src/command.h
+++ app/src/command.h
@@ -3,33 +3,33 @@
 
 #include <inttypes.h>
 #include <SDL2/SDL_stdinc.h>
-#include <SDL2/SDL_platform.h>
 
-// <https://stackoverflow.com/a/44383330/1987178>
 #ifdef _WIN32
+
+# include <winsock2.h> // not needed here, but must never be included AFTER windows.h
+# include <windows.h>
 # define PRIexitcode "lu"
+// <https://stackoverflow.com/a/44383330/1987178>
 # ifdef _WIN64
 #   define PRIsizet PRIu64
 # else
 #   define PRIsizet PRIu32
 # endif
-#else
-# define PRIsizet "zu"
-# define PRIexitcode "d"
-#endif
-
-#ifdef __WINDOWS__
-# include <winsock2.h> // not needed here, but must never be included AFTER windows.h
-# include <windows.h>
 # define PROCESS_NONE NULL
   typedef HANDLE process_t;
   typedef DWORD exit_code_t;
+
 #else
+
 # include <sys/types.h>
+# define PRIsizet "zu"
+# define PRIexitcode "d"
 # define PROCESS_NONE -1
   typedef pid_t process_t;
   typedef int exit_code_t;
+
 #endif
+
 # define NO_EXIT_CODE -1
 
 enum process_result {
