7fc8793d5b98a8a7c2cf208227c5cb966513171d
Romain Vimont <rom@rom1v.com>
2019-05-29 17:45:46 +0200

    Make buffer util functions accept const buffers
    So that they can be used both on const and non-const input buffers.

--- app/src/buffer_util.h
+++ app/src/buffer_util.h
@@ -19,12 +19,12 @@ buffer_write32be(uint8_t *buf, uint32_t value) {
 }
 
 static inline uint32_t
-buffer_read32be(uint8_t *buf) {
+buffer_read32be(const uint8_t *buf) {
     return (buf[0] << 24) | (buf[1] << 16) | (buf[2] << 8) | buf[3];
 }
 
 static inline
-uint64_t buffer_read64be(uint8_t *buf) {
+uint64_t buffer_read64be(const uint8_t *buf) {
     uint32_t msb = buffer_read32be(buf);
     uint32_t lsb = buffer_read32be(&buf[4]);
     return ((uint64_t) msb << 32) | lsb;
