5d1133925994077d93d37e95565c359e026abfff
Romain Vimont <rom@rom1v.com>
2019-06-07 17:19:00 +0200

    Inline lock_util functions
    They are just tiny wrappers.

--- app/src/lock_util.h
+++ app/src/lock_util.h
@@ -2,25 +2,50 @@
 #define LOCKUTIL_H
 
 #include <stdint.h>
+#include <SDL2/SDL_mutex.h>
 
-// forward declarations
-typedef struct SDL_mutex SDL_mutex;
-typedef struct SDL_cond SDL_cond;
+#include "log.h"
 
-void
-mutex_lock(SDL_mutex *mutex);
+static inline void
+mutex_lock(SDL_mutex *mutex) {
+    if (SDL_LockMutex(mutex)) {
+        LOGC("Could not lock mutex");
+        abort();
+    }
+}
 
-void
-mutex_unlock(SDL_mutex *mutex);
+static inline void
+mutex_unlock(SDL_mutex *mutex) {
+    if (SDL_UnlockMutex(mutex)) {
+        LOGC("Could not unlock mutex");
+        abort();
+    }
+}
 
-void
-cond_wait(SDL_cond *cond, SDL_mutex *mutex);
+static inline void
+cond_wait(SDL_cond *cond, SDL_mutex *mutex) {
+    if (SDL_CondWait(cond, mutex)) {
+        LOGC("Could not wait on condition");
+        abort();
+    }
+}
 
-// return 0 or SDL_MUTEX_TIMEDOUT
-int
-cond_wait_timeout(SDL_cond *cond, SDL_mutex *mutex, uint32_t ms);
+static inline int
+cond_wait_timeout(SDL_cond *cond, SDL_mutex *mutex, uint32_t ms) {
+    int r = SDL_CondWaitTimeout(cond, mutex, ms);
+    if (r < 0) {
+        LOGC("Could not wait on condition with timeout");
+        abort();
+    }
+    return r;
+}
 
-void
-cond_signal(SDL_cond *cond);
+static inline void
+cond_signal(SDL_cond *cond) {
+    if (SDL_CondSignal(cond)) {
+        LOGC("Could not signal a condition");
+        abort();
+    }
+}
 
 #endif
