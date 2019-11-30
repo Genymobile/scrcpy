8e15c1d733c0019758b030c5c37886932fce1800
Yu-Chen Lin <npes87184@gmail.com>
2019-11-30 12:33:40 +0800

    Correct coding style
    Signed-off-by: Yu-Chen Lin <npes87184@gmail.com>

--- app/src/util/buffer_util.h
+++ app/src/util/buffer_util.h
@@ -36,8 +36,8 @@ buffer_read32be(const uint8_t *buf) {
     return (buf[0] << 24) | (buf[1] << 16) | (buf[2] << 8) | buf[3];
 }
 
-static inline
-uint64_t buffer_read64be(const uint8_t *buf) {
+static inline uint64_t
+buffer_read64be(const uint8_t *buf) {
     uint32_t msb = buffer_read32be(buf);
     uint32_t lsb = buffer_read32be(&buf[4]);
     return ((uint64_t) msb << 32) | lsb;
