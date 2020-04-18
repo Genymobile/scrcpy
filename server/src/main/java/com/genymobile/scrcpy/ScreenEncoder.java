125c5561e8f4e169fe52884e47fae1f3b9ffb9f7
Tzah Mazuz <tzah.mazuz@testproject.io>
2020-04-18 02:13:19 +0200

    Use MediaFormat constant for MIME type
    Replace "video/avc" by MIMETYPE_VIDEO_AVC.
    
    Signed-off-by: Romain Vimont <rom@rom1v.com>

--- server/src/main/java/com/genymobile/scrcpy/ScreenEncoder.java
+++ server/src/main/java/com/genymobile/scrcpy/ScreenEncoder.java
@@ -136,12 +136,12 @@ public class ScreenEncoder implements Device.RotationListener {
     }
 
     private static MediaCodec createCodec() throws IOException {
-        return MediaCodec.createEncoderByType("video/avc");
+        return MediaCodec.createEncoderByType(MediaFormat.MIMETYPE_VIDEO_AVC);
     }
 
     private static MediaFormat createFormat(int bitRate, int maxFps, int iFrameInterval) {
         MediaFormat format = new MediaFormat();
-        format.setString(MediaFormat.KEY_MIME, "video/avc");
+        format.setString(MediaFormat.KEY_MIME, MediaFormat.MIMETYPE_VIDEO_AVC);
         format.setInteger(MediaFormat.KEY_BIT_RATE, bitRate);
         // must be present to configure the encoder, but does not impact the actual frame rate, which is variable
         format.setInteger(MediaFormat.KEY_FRAME_RATE, 60);
