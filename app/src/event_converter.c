ad78ca9cfb166dea9f7c7fd544d0366eabaef101
Romain Vimont <rom@rom1v.com>
2020-05-28 09:57:31 +0200

    Forward Shift to the device
    This allows to select text with Shift + arrow keys.
    
    Fixes #942 <https://github.com/Genymobile/scrcpy/issues/942>.

--- app/src/event_converter.c
+++ app/src/event_converter.c
@@ -93,6 +93,8 @@ convert_keycode(SDL_Keycode from, enum android_keycode *to, uint16_t mod,
         MAP(SDLK_DOWN,         AKEYCODE_DPAD_DOWN);
         MAP(SDLK_UP,           AKEYCODE_DPAD_UP);
         MAP(SDLK_RCTRL,        AKEYCODE_CTRL_RIGHT);
+        MAP(SDLK_LSHIFT,       AKEYCODE_SHIFT_LEFT);
+        MAP(SDLK_RSHIFT,       AKEYCODE_SHIFT_RIGHT);
     }
 
     if (!(mod & (KMOD_NUM | KMOD_SHIFT))) {
