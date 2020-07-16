cfa284412102c695213eea6a65ba500f1eafa9f8
Romain Vimont <rom@rom1v.com>
2020-07-17 00:00:42 +0200

    Use Ctrl for all shortcuts
    Remove the Cmd modifier on macOS, which was possible only for some
    shortcuts but not all.
    
    This paves the way to make the shortcut modifier customizable.

--- app/src/input_manager.c
+++ app/src/input_manager.c
@@ -264,27 +264,16 @@ input_manager_process_key(struct input_manager *im,
     bool alt = event->keysym.mod & (KMOD_LALT | KMOD_RALT);
     bool meta = event->keysym.mod & (KMOD_LGUI | KMOD_RGUI);
 
-    // use Cmd on macOS, Ctrl on other platforms
-#ifdef __APPLE__
-    bool cmd = !ctrl && meta;
-#else
-    if (meta) {
-        // no shortcuts involve Meta on platforms other than macOS, and it must
-        // not be forwarded to the device
-        return;
-    }
-    bool cmd = ctrl; // && !meta, already guaranteed
-#endif
-
-    if (alt) {
-        // no shortcuts involve Alt, and it must not be forwarded to the device
+    if (alt || meta) {
+        // no shortcuts involve Alt or Meta, and they must not be forwarded to
+        // the device
         return;
     }
 
     struct controller *controller = im->controller;
 
     // capture all Ctrl events
-    if (ctrl || cmd) {
+    if (ctrl) {
         SDL_Keycode keycode = event->keysym.sym;
         bool down = event->type == SDL_KEYDOWN;
         int action = down ? ACTION_DOWN : ACTION_UP;
@@ -292,37 +281,33 @@ input_manager_process_key(struct input_manager *im,
         bool shift = event->keysym.mod & (KMOD_LSHIFT | KMOD_RSHIFT);
         switch (keycode) {
             case SDLK_h:
-                // Ctrl+h on all platform, since Cmd+h is already captured by
-                // the system on macOS to hide the window
-                if (control && ctrl && !meta && !shift && !repeat) {
+                if (control && ctrl && !shift && !repeat) {
                     action_home(controller, action);
                 }
                 return;
             case SDLK_b: // fall-through
             case SDLK_BACKSPACE:
-                if (control && cmd && !shift && !repeat) {
+                if (control && ctrl && !shift && !repeat) {
                     action_back(controller, action);
                 }
                 return;
             case SDLK_s:
-                if (control && cmd && !shift && !repeat) {
+                if (control && ctrl && !shift && !repeat) {
                     action_app_switch(controller, action);
                 }
                 return;
             case SDLK_m:
-                // Ctrl+m on all platform, since Cmd+m is already captured by
-                // the system on macOS to minimize the window
-                if (control && ctrl && !meta && !shift && !repeat) {
+                if (control && ctrl && !shift && !repeat) {
                     action_menu(controller, action);
                 }
                 return;
             case SDLK_p:
-                if (control && cmd && !shift && !repeat) {
+                if (control && ctrl && !shift && !repeat) {
                     action_power(controller, action);
                 }
                 return;
             case SDLK_o:
-                if (control && cmd && !repeat && down) {
+                if (control && ctrl && !repeat && down) {
                     enum screen_power_mode mode = shift
                                                 ? SCREEN_POWER_MODE_NORMAL
                                                 : SCREEN_POWER_MODE_OFF;
@@ -330,34 +315,34 @@ input_manager_process_key(struct input_manager *im,
                 }
                 return;
             case SDLK_DOWN:
-                if (control && cmd && !shift) {
+                if (control && ctrl && !shift) {
                     // forward repeated events
                     action_volume_down(controller, action);
                 }
                 return;
             case SDLK_UP:
-                if (control && cmd && !shift) {
+                if (control && ctrl && !shift) {
                     // forward repeated events
                     action_volume_up(controller, action);
                 }
                 return;
             case SDLK_LEFT:
-                if (cmd && !shift && !repeat && down) {
+                if (ctrl && !shift && !repeat && down) {
                     rotate_client_left(im->screen);
                 }
                 return;
             case SDLK_RIGHT:
-                if (cmd && !shift && !repeat && down) {
+                if (ctrl && !shift && !repeat && down) {
                     rotate_client_right(im->screen);
                 }
                 return;
             case SDLK_c:
-                if (control && cmd && !shift && !repeat && down) {
+                if (control && ctrl && !shift && !repeat && down) {
                     request_device_clipboard(controller);
                 }
                 return;
             case SDLK_v:
-                if (control && cmd && !repeat && down) {
+                if (control && ctrl && !repeat && down) {
                     if (shift) {
                         // store the text in the device clipboard and paste
                         set_device_clipboard(controller, true);
@@ -368,29 +353,29 @@ input_manager_process_key(struct input_manager *im,
                 }
                 return;
             case SDLK_f:
-                if (cmd && !shift && !repeat && down) {
+                if (ctrl && !shift && !repeat && down) {
                     screen_switch_fullscreen(im->screen);
                 }
                 return;
             case SDLK_x:
-                if (cmd && !shift && !repeat && down) {
+                if (ctrl && !shift && !repeat && down) {
                     screen_resize_to_fit(im->screen);
                 }
                 return;
             case SDLK_g:
-                if (cmd && !shift && !repeat && down) {
+                if (ctrl && !shift && !repeat && down) {
                     screen_resize_to_pixel_perfect(im->screen);
                 }
                 return;
             case SDLK_i:
-                if (cmd && !shift && !repeat && down) {
+                if (ctrl && !shift && !repeat && down) {
                     struct fps_counter *fps_counter =
                         im->video_buffer->fps_counter;
                     switch_fps_counter_state(fps_counter);
                 }
                 return;
             case SDLK_n:
-                if (control && cmd && !repeat && down) {
+                if (control && ctrl && !repeat && down) {
                     if (shift) {
                         collapse_notification_panel(controller);
                     } else {
@@ -399,7 +384,7 @@ input_manager_process_key(struct input_manager *im,
                 }
                 return;
             case SDLK_r:
-                if (control && cmd && !shift && !repeat && down) {
+                if (control && ctrl && !shift && !repeat && down) {
                     rotate_device(controller);
                 }
                 return;
