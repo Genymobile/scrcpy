25b80a85f1bb024bd01d097189d44f663966ac38
Romain Vimont <rom@rom1v.com>
2020-07-17 00:00:42 +0200

    Set device clipboard only if necessary
    Do not explicitly set the clipboard text if it already contains the
    expected content. This avoids possible copy-paste loops between the
    computer and the device.

--- server/src/main/java/com/genymobile/scrcpy/Device.java
+++ server/src/main/java/com/genymobile/scrcpy/Device.java
@@ -221,6 +221,16 @@ public final class Device {
         if (clipboardManager == null) {
             return false;
         }
+
+        String currentClipboard = getClipboardText();
+        if (currentClipboard == null || currentClipboard.equals(text)) {
+            // The clipboard already contains the requested text.
+            // Since pasting text from the computer involves setting the device clipboard, it could be set twice on a copy-paste. This would cause
+            // the clipboard listeners to be notified twice, and that would flood the Android keyboard clipboard history. To workaround this
+            // problem, do not explicitly set the clipboard text if it already contains the expected content.
+            return false;
+        }
+
         isSettingClipboard.set(true);
         boolean ok = clipboardManager.setText(text);
         isSettingClipboard.set(false);
