d41583e1a2d3b37b3fe76e098d3060b96e3e0930
Hunt Lin <hunt.lin@tes-tec.com>
2020-11-03 13:01:43 +0800

    Force to use opengl

--- app/src/screen.c
+++ app/src/screen.c
@@ -258,6 +258,8 @@ screen_init_rendering(struct screen *screen, const char *window_title,
         window_flags |= SDL_WINDOW_BORDERLESS;
     }
 
+    SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
+
     int x = window_x != SC_WINDOW_POSITION_UNDEFINED
           ? window_x : (int) SDL_WINDOWPOS_UNDEFINED;
     int y = window_y != SC_WINDOW_POSITION_UNDEFINED
