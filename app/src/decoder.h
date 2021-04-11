3fb0594fe88d173a24bec6bd8d5e46f02d7c1adb
Romain Vimont <rom@rom1v.com>
2021-04-11 15:01:05 +0200

    Expose decoder as packet sink
    Make decoder implement the packet sink trait.
    
    This will allow the stream to push packets without depending on the
    concrete sink type.

--- app/src/decoder.h
+++ app/src/decoder.h
@@ -3,12 +3,16 @@
 
 #include "common.h"
 
+#include "trait/packet_sink.h"
+
 #include <stdbool.h>
 #include <libavformat/avformat.h>
 
 struct video_buffer;
 
 struct decoder {
+    struct sc_packet_sink packet_sink; // packet sink trait
+
     struct video_buffer *video_buffer;
 
     AVCodecContext *codec_ctx;
