aaf7875d923e492047e66ca88585079704abf5ad
Frank Leon Rose <frankleonrose@users.noreply.github.com>
2021-04-22 13:59:46 -0400

    Ensure get_server_path() retval is freeable
    PR #2276 <https://github.com/Genymobile/scrcpy/pull/2276>
    
    Signed-off-by: Romain Vimont <rom@rom1v.com>

--- app/src/server.c
+++ app/src/server.c
@@ -58,7 +58,7 @@ get_server_path(void) {
         LOGE("Could not get executable path, "
              "using " SERVER_FILENAME " from current directory");
         // not found, use current directory
-        return SERVER_FILENAME;
+        return strdup(SERVER_FILENAME);
     }
     char *dir = dirname(executable_path);
     size_t dirlen = strlen(dir);
@@ -70,7 +70,7 @@ get_server_path(void) {
         LOGE("Could not alloc server path string, "
              "using " SERVER_FILENAME " from current directory");
         free(executable_path);
-        return SERVER_FILENAME;
+        return strdup(SERVER_FILENAME);
     }
 
     memcpy(server_path, dir, dirlen);
