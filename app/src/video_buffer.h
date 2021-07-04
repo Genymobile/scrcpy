79278961b9955173573f2af884fc468d1ec67380
Romain Vimont <rom@rom1v.com>
2021-07-04 15:24:51 +0200

    Implement buffering
    To minimize latency (at the cost of jitter), scrcpy always displays a
    frame as soon as it available, without waiting.
    
    However, when recording (--record), it still writes the captured
    timestamps to the output file, so that the recorded file can be played
    correctly without jitter.
    
    Some real-time use cases might benefit from adding a small latency to
    compensate for jitter too. For example, few tens of seconds of latency
    for live-streaming are not important, but jitter is noticeable.
    
    Therefore, implement a buffering mechanism (disabled by default) to add
    a configurable latency delay.
    
    PR #2417 <https://github.com/Genymobile/scrcpy/issues/2417>

--- app/src/video_buffer.h
+++ app/src/video_buffer.h
@@ -5,13 +5,38 @@
 
 #include <stdbool.h>
 
+#include "clock.h"
 #include "frame_buffer.h"
+#include "util/queue.h"
+#include "util/thread.h"
+#include "util/tick.h"
 
 // forward declarations
 typedef struct AVFrame AVFrame;
 
+struct sc_video_buffer_frame {
+    AVFrame *frame;
+    struct sc_video_buffer_frame *next;
+};
+
+struct sc_video_buffer_frame_queue SC_QUEUE(struct sc_video_buffer_frame);
+
 struct sc_video_buffer {
     struct sc_frame_buffer fb;
+
+    sc_tick buffering_time;
+
+    // only if buffering_time > 0
+    struct {
+        sc_thread thread;
+        sc_mutex mutex;
+        sc_cond queue_cond;
+        sc_cond wait_cond;
+
+        struct sc_clock clock;
+        struct sc_video_buffer_frame_queue queue;
+        bool stopped;
+    } b; // buffering
 
     const struct sc_video_buffer_callbacks *cbs;
     void *cbs_userdata;
@@ -23,9 +48,18 @@ struct sc_video_buffer_callbacks {
 };
 
 bool
-sc_video_buffer_init(struct sc_video_buffer *vb,
+sc_video_buffer_init(struct sc_video_buffer *vb, sc_tick buffering_time,
                      const struct sc_video_buffer_callbacks *cbs,
                      void *cbs_userdata);
+
+bool
+sc_video_buffer_start(struct sc_video_buffer *vb);
+
+void
+sc_video_buffer_stop(struct sc_video_buffer *vb);
+
+void
+sc_video_buffer_join(struct sc_video_buffer *vb);
 
 void
 sc_video_buffer_destroy(struct sc_video_buffer *vb);
