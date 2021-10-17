156d907a82b3f503b664c718fe3c90da9af243e0
Romain Vimont <rom@rom1v.com>
2021-10-17 16:14:33 +0200

    Expose adb execution with redirection
    Expose the redirection feature to the adb API.

--- app/src/adb.h
+++ app/src/adb.h
@@ -12,6 +12,11 @@ process_t
 adb_execute(const char *serial, const char *const adb_cmd[], size_t len);
 
 process_t
+adb_execute_redirect(const char *serial, const char *const adb_cmd[],
+                     size_t len, pipe_t *pipe_stdin, pipe_t *pipe_stdout,
+                     pipe_t *pipe_stderr);
+
+process_t
 adb_forward(const char *serial, uint16_t local_port,
             const char *device_socket_name);
 
