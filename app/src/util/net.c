07d75eb336264cab4eb4cabfde482ce0104eba2b
Romain Vimont <rom@rom1v.com>
2021-10-17 16:21:01 +0200

    Simplify net_send_all()
    There is no need to declare the variable before the loop.

--- app/src/util/net.c
+++ app/src/util/net.c
@@ -116,9 +116,8 @@ net_send(socket_t socket, const void *buf, size_t len) {
 ssize_t
 net_send_all(socket_t socket, const void *buf, size_t len) {
     size_t copied = 0;
-    ssize_t w = 0;
     while (len > 0) {
-        w = send(socket, buf, len, 0);
+        ssize_t w = send(socket, buf, len, 0);
         if (w == -1) {
             return copied ? (ssize_t) copied : -1;
         }
