eb6afe76698e3d974bae4615c0cba2e2744183de
Romain Vimont <rom@rom1v.com>
2021-10-26 22:49:45 +0200

    Move net_init() and net_cleanup() upwards
    These two functions are global, define them at the top of the
    implementation file. This is consistent with the header file.

--- app/src/util/net.c
+++ app/src/util/net.c
@@ -19,6 +19,26 @@
   typedef struct in_addr IN_ADDR;
 #endif
 
+bool
+net_init(void) {
+#ifdef __WINDOWS__
+    WSADATA wsa;
+    int res = WSAStartup(MAKEWORD(2, 2), &wsa) < 0;
+    if (res < 0) {
+        LOGC("WSAStartup failed with error %d", res);
+        return false;
+    }
+#endif
+    return true;
+}
+
+void
+net_cleanup(void) {
+#ifdef __WINDOWS__
+    WSACleanup();
+#endif
+}
+
 static void
 net_perror(const char *s) {
 #ifdef _WIN32
@@ -131,26 +151,6 @@ net_send_all(socket_t socket, const void *buf, size_t len) {
 bool
 net_shutdown(socket_t socket, int how) {
     return !shutdown(socket, how);
-}
-
-bool
-net_init(void) {
-#ifdef __WINDOWS__
-    WSADATA wsa;
-    int res = WSAStartup(MAKEWORD(2, 2), &wsa) < 0;
-    if (res < 0) {
-        LOGC("WSAStartup failed with error %d", res);
-        return false;
-    }
-#endif
-    return true;
-}
-
-void
-net_cleanup(void) {
-#ifdef __WINDOWS__
-    WSACleanup();
-#endif
 }
 
 bool
