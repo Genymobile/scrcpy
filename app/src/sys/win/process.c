143bfc03950f05fe27dd536eef3d3fd51a2ade57
Romain Vimont <rom@rom1v.com>
2021-11-19 22:42:49 +0100

    Expose inherit flag for process execution
    Let the caller decide if stdout and stderr must be inherited on process
    creation, i.e. if stdout and stderr of the child process should be
    printed in the scrcpy console.
    
    This allows to get output and errors for specific adb commands depending
    on the context.

--- app/src/sys/win/process.c
+++ app/src/sys/win/process.c
@@ -27,12 +27,22 @@ build_cmd(char *cmd, size_t len, const char *const argv[]) {
 }
 
 enum sc_process_result
-sc_process_execute_p(const char *const argv[], HANDLE *handle,
+sc_process_execute_p(const char *const argv[], HANDLE *handle, unsigned inherit,
                      HANDLE *pin, HANDLE *pout, HANDLE *perr) {
-    enum sc_process_result ret = SC_PROCESS_ERROR_GENERIC;
+    bool inherit_stdout = inherit & SC_STDOUT;
+    bool inherit_stderr = inherit & SC_STDERR;
+
+    // If pout is defined, then inherit MUST NOT contain SC_STDOUT.
+    assert(!pout || !inherit_stdout);
+    // If perr is defined, then inherit MUST NOT contain SC_STDERR.
+    assert(!perr || !inherit_stderr);
 
     // Add 1 per non-NULL pointer
-    unsigned handle_count = !!pin + !!pout + !!perr;
+    unsigned handle_count = !!pin
+                          + (pout || inherit_stdout)
+                          + (perr || inherit_stderr);
+
+    enum sc_process_result ret = SC_PROCESS_ERROR_GENERIC;
 
     SECURITY_ATTRIBUTES sa;
     sa.nLength = sizeof(SECURITY_ATTRIBUTES);
@@ -82,18 +92,19 @@ sc_process_execute_p(const char *const argv[], HANDLE *handle,
     LPPROC_THREAD_ATTRIBUTE_LIST lpAttributeList = NULL;
     if (handle_count) {
         si.StartupInfo.dwFlags = STARTF_USESTDHANDLES;
-
         unsigned i = 0;
         if (pin) {
             si.StartupInfo.hStdInput = stdin_read_handle;
             handles[i++] = si.StartupInfo.hStdInput;
         }
-        if (pout) {
-            si.StartupInfo.hStdOutput = stdout_write_handle;
+        if (pout || inherit_stdout) {
+            si.StartupInfo.hStdOutput = pout ? stdout_write_handle
+                                             : GetStdHandle(STD_OUTPUT_HANDLE);
             handles[i++] = si.StartupInfo.hStdOutput;
         }
-        if (perr) {
-            si.StartupInfo.hStdError = stderr_write_handle;
+        if (perr || inherit_stderr) {
+            si.StartupInfo.hStdError = perr ? stderr_write_handle
+                                            : GetStdHandle(STD_ERROR_HANDLE);
             handles[i++] = si.StartupInfo.hStdError;
         }
 
@@ -141,7 +152,9 @@ sc_process_execute_p(const char *const argv[], HANDLE *handle,
     }
 
     BOOL bInheritHandles = handle_count > 0;
-    DWORD dwCreationFlags = handle_count > 0 ? EXTENDED_STARTUPINFO_PRESENT : 0;
+    // DETACHED_PROCESS to disable stdin, stdout and stderr
+    DWORD dwCreationFlags = handle_count == 0 ? DETACHED_PROCESS
+                                              : EXTENDED_STARTUPINFO_PRESENT;
     BOOL ok = CreateProcessW(NULL, wide, NULL, NULL, bInheritHandles,
                              dwCreationFlags, NULL, NULL, &si.StartupInfo, &pi);
     free(wide);
