dec9e436c21c02a345084bcee21457ed00d811b1
Romain Vimont <rom@rom1v.com>
2021-12-28 11:50:24 +0100

    Add CLAMP() macro

--- app/src/common.h
+++ app/src/common.h
@@ -7,6 +7,7 @@
 #define ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
 #define MIN(X,Y) (X) < (Y) ? (X) : (Y)
 #define MAX(X,Y) (X) > (Y) ? (X) : (Y)
+#define CLAMP(V,X,Y) MIN( MAX((V),(X)), (Y) )
 
 #define container_of(ptr, type, member) \
     ((type *) (((char *) (ptr)) - offsetof(type, member)))
