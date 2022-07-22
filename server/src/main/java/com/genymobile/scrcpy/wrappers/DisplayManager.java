ac7ffeb91c1202bcdd1863457e7164dfad0889fb
Sergey Volkov <drauggres@gmail.com>
2022-07-22 18:44:10 +0300

    fix: workaround for getDisplayIds on Android 13

--- server/src/main/java/com/genymobile/scrcpy/wrappers/DisplayManager.java
+++ server/src/main/java/com/genymobile/scrcpy/wrappers/DisplayManager.java
@@ -1,9 +1,13 @@
 package com.genymobile.scrcpy.wrappers;
 
 import com.genymobile.scrcpy.DisplayInfo;
+import com.genymobile.scrcpy.Ln;
 import com.genymobile.scrcpy.Size;
 
 import android.os.IInterface;
+import android.view.Display;
+
+import java.lang.reflect.Method;
 
 public final class DisplayManager {
     private final IInterface manager;
@@ -34,6 +38,14 @@ public final class DisplayManager {
     public int[] getDisplayIds() {
         try {
             return (int[]) manager.getClass().getMethod("getDisplayIds").invoke(manager);
+        } catch (NoSuchMethodException e) {
+            Ln.d("Failed to get display ids");
+            Ln.d("Available methods:");
+            for (Method m: manager.getClass().getMethods()) {
+                Ln.d(m.getName());
+            }
+            Ln.d("///");
+            return new int[]{Display.DEFAULT_DISPLAY};
         } catch (Exception e) {
             throw new AssertionError(e);
         }
