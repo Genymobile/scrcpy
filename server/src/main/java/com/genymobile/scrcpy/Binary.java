3848ce86f118496467ebd91b40dbf571e2a29e24
Romain Vimont <rom@rom1v.com>
2022-07-24 16:00:26 +0200

    Extract conversion from u16 fixed-point to float
    PR #3369 <https://github.com/Genymobile/scrcpy/pull/3369>

--- server/src/main/java/com/genymobile/scrcpy/Binary.java
+++ server/src/main/java/com/genymobile/scrcpy/Binary.java
@@ -12,4 +12,16 @@ public final class Binary {
     public static int toUnsigned(byte value) {
         return value & 0xff;
     }
+
+    /**
+     * Convert unsigned 16-bit fixed-point to a float between 0 and 1
+     *
+     * @param value encoded value
+     * @return Float value between 0 and 1
+     */
+    public static float u16FixedPointToFloat(short value) {
+        int unsignedShort = Binary.toUnsigned(value);
+        // 0x1p16f is 2^16 as float
+        return unsignedShort == 0xffff ? 1f : (unsignedShort / 0x1p16f);
+    }
 }
