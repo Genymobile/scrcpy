949b64dff2efe23039fa4801fbc95ef46e208477
SeungHoon Han <senicy.han@samsung.com>
2022-08-02 19:39:50 +0900

    Add fallback to get DisplayInfo
    PR #3416 <https://github.com/Genymobile/scrcpy/pull/3416>
    
    Signed-off-by: Romain Vimont <rom@rom1v.com>

--- server/src/main/java/com/genymobile/scrcpy/wrappers/DisplayManager.java
+++ server/src/main/java/com/genymobile/scrcpy/wrappers/DisplayManager.java
@@ -1,7 +1,15 @@
 package com.genymobile.scrcpy.wrappers;
 
+import com.genymobile.scrcpy.Command;
 import com.genymobile.scrcpy.DisplayInfo;
+import com.genymobile.scrcpy.Ln;
 import com.genymobile.scrcpy.Size;
+
+import android.view.Display;
+
+import java.lang.reflect.Field;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class DisplayManager {
     private final Object manager; // instance of hidden class android.hardware.display.DisplayManagerGlobal
@@ -10,11 +18,61 @@ public final class DisplayManager {
         this.manager = manager;
     }
 
+    // public to call it from unit tests
+    public static DisplayInfo parseDisplayInfo(String dumpsysDisplayOutput, int displayId) {
+        Pattern regex = Pattern.compile(
+                "^    mOverrideDisplayInfo=DisplayInfo\\{\".*?\", displayId " + displayId + ".*?(, FLAG_.*)?, real ([0-9]+) x ([0-9]+).*?, "
+                        + "rotation ([0-9]+).*?, layerStack ([0-9]+)",
+                Pattern.MULTILINE);
+        Matcher m = regex.matcher(dumpsysDisplayOutput);
+        if (!m.find()) {
+            return null;
+        }
+        int flags = parseDisplayFlags(m.group(1));
+        int width = Integer.parseInt(m.group(2));
+        int height = Integer.parseInt(m.group(3));
+        int rotation = Integer.parseInt(m.group(4));
+        int layerStack = Integer.parseInt(m.group(5));
+
+        return new DisplayInfo(displayId, new Size(width, height), rotation, layerStack, flags);
+    }
+
+    private static DisplayInfo getDisplayInfoFromDumpsysDisplay(int displayId) {
+        try {
+            String dumpsysDisplayOutput = Command.execReadOutput("dumpsys", "display");
+            return parseDisplayInfo(dumpsysDisplayOutput, displayId);
+        } catch (Exception e) {
+            Ln.e("Could not get display info from \"dumpsys display\" output", e);
+            return null;
+        }
+    }
+
+    private static int parseDisplayFlags(String text) {
+        Pattern regex = Pattern.compile("FLAG_[A-Z_]+");
+        if (text == null) {
+            return 0;
+        }
+
+        int flags = 0;
+        Matcher m = regex.matcher(text);
+        while (m.find()) {
+            String flagString = m.group();
+            try {
+                Field filed = Display.class.getDeclaredField(flagString);
+                flags |= filed.getInt(null);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                // Silently ignore, some flags reported by "dumpsys display" are @TestApi
+            }
+        }
+        return flags;
+    }
+
     public DisplayInfo getDisplayInfo(int displayId) {
         try {
             Object displayInfo = manager.getClass().getMethod("getDisplayInfo", int.class).invoke(manager, displayId);
             if (displayInfo == null) {
-                return null;
+                // fallback when displayInfo is null
+                return getDisplayInfoFromDumpsysDisplay(displayId);
             }
             Class<?> cls = displayInfo.getClass();
             // width and height already take the rotation into account
