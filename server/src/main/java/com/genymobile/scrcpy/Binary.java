5b8e9aa0e95dd5e4aa234dfcbd0445005d197bf0
Romain Vimont <rom@rom1v.com>
2022-08-03 14:51:07 +0200

    Move toUnsigned() to a Binary util class
    PR #3369 <https://github.com/Genymobile/scrcpy/pull/3369>

---
+++ server/src/main/java/com/genymobile/scrcpy/Binary.java
@@ -0,0 +1,15 @@
+package com.genymobile.scrcpy;
+
+public final class Binary {
+    private Binary() {
+        // not instantiable
+    }
+
+    public static int toUnsigned(short value) {
+        return value & 0xffff;
+    }
+
+    public static int toUnsigned(byte value) {
+        return value & 0xff;
+    }
+}
