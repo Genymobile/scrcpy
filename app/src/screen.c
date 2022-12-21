18082f60697ae8edad2db950637638c8bb6bf0d2
Romain Vimont <rom@rom1v.com>
2022-12-21 22:06:43 +0100

    Remove continuous resizing workaround for Windows
    It turns out that the workaround only worked for MacOS.
    
    Refs #3458 <https://github.com/Genymobile/scrcpy/issues/3458>
    Refs SDL/#1059 <https://github.com/libsdl-org/SDL/issues/1059>

--- app/src/screen.c
+++ app/src/screen.c
@@ -306,13 +306,14 @@ sc_screen_render(struct sc_screen *screen, bool update_content_rect) {
 }
 
 
-#if defined(__APPLE__) || defined(__WINDOWS__)
+#if defined(__APPLE__)
 # define CONTINUOUS_RESIZING_WORKAROUND
 #endif
 
 #ifdef CONTINUOUS_RESIZING_WORKAROUND
 // On Windows and MacOS, resizing blocks the event loop, so resizing events are
-// not triggered. As a workaround, handle them in an event handler.
+// not triggered. On MacOS, as a workaround, handle them in an event handler
+// (it does not work for Windows unfortunately).
 //
 // <https://bugzilla.libsdl.org/show_bug.cgi?id=2077>
 // <https://stackoverflow.com/a/40693139/1987178>
