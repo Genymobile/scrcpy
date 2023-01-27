72cd233a4e8b27bd2b1ec41e28cf6df0a80d4d77
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2023-01-27 20:13:37 +0800

    Add a fake Android Context
    Since scrcpy-server is not an Android application (it's a java
    executable), it has no Context.
    
    Some features will require a Context instance to get the package name
    and the UID. Add a FakeContext for this purpose.
    
    Co-authored-by: Romain Vimont <rom@rom1v.com>
    Signed-off-by: Romain Vimont <rom@rom1v.com>

---
+++ server/src/main/java/com/genymobile/scrcpy/FakeContext.java
@@ -0,0 +1,40 @@
+package com.genymobile.scrcpy;
+
+import android.annotation.TargetApi;
+import android.content.AttributionSource;
+import android.content.ContextWrapper;
+import android.os.Build;
+import android.os.Process;
+
+public final class FakeContext extends ContextWrapper {
+
+    public static final String PACKAGE_NAME = "com.android.shell";
+
+    private static final FakeContext INSTANCE = new FakeContext();
+
+    public static FakeContext get() {
+        return INSTANCE;
+    }
+
+    private FakeContext() {
+        super(null);
+    }
+
+    @Override
+    public String getPackageName() {
+        return PACKAGE_NAME;
+    }
+
+    @Override
+    public String getOpPackageName() {
+        return PACKAGE_NAME;
+    }
+
+    @TargetApi(Build.VERSION_CODES.S)
+    @Override
+    public AttributionSource getAttributionSource() {
+        AttributionSource.Builder builder = new AttributionSource.Builder(Process.SHELL_UID);
+        builder.setPackageName(PACKAGE_NAME);
+        return builder.build();
+    }
+}
