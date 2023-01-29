517de10ac375939a828884cbbe25937068872f79
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2023-01-30 00:42:09 +0800

    Add workaround to capture audio on Android 11
    On Android 11, it is possible to start the capture only when the running
    app is in foreground. But scrcpy is not an app, it's a Java application
    started from shell.
    
    As a workaround, start an existing Android shell existing activity just
    to start the capture, then close it immediately.
    
    PR #3757 <https://github.com/Genymobile/scrcpy/pull/3757>
    
    Co-authored-by: Romain Vimont <rom@rom1v.com>
    Signed-off-by: Romain Vimont <rom@rom1v.com>

--- server/src/main/java/com/genymobile/scrcpy/wrappers/ActivityManager.java
+++ server/src/main/java/com/genymobile/scrcpy/wrappers/ActivityManager.java
@@ -3,7 +3,12 @@ package com.genymobile.scrcpy.wrappers;
 import com.genymobile.scrcpy.FakeContext;
 import com.genymobile.scrcpy.Ln;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.IInterface;
 
@@ -11,12 +16,15 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+@SuppressLint("PrivateApi,DiscouragedPrivateApi")
 public class ActivityManager {
 
     private final IInterface manager;
     private Method getContentProviderExternalMethod;
     private boolean getContentProviderExternalMethodNewVersion = true;
     private Method removeContentProviderExternalMethod;
+    private Method startActivityAsUserWithFeatureMethod;
+    private Method forceStopPackageMethod;
 
     public ActivityManager(IInterface manager) {
         this.manager = manager;
@@ -43,6 +51,7 @@ public class ActivityManager {
         return removeContentProviderExternalMethod;
     }
 
+    @TargetApi(Build.VERSION_CODES.Q)
     private ContentProvider getContentProviderExternal(String name, IBinder token) {
         try {
             Method method = getGetContentProviderExternalMethod();
@@ -84,5 +93,56 @@ public class ActivityManager {
 
     public ContentProvider createSettingsProvider() {
         return getContentProviderExternal("settings", new Binder());
+    }
+
+    private Method getStartActivityAsUserWithFeatureMethod() throws NoSuchMethodException, ClassNotFoundException {
+        if (startActivityAsUserWithFeatureMethod == null) {
+            Class<?> iApplicationThreadClass = Class.forName("android.app.IApplicationThread");
+            Class<?> profilerInfo = Class.forName("android.app.ProfilerInfo");
+            startActivityAsUserWithFeatureMethod = manager.getClass()
+                    .getMethod("startActivityAsUserWithFeature", iApplicationThreadClass, String.class, String.class, Intent.class, String.class,
+                            IBinder.class, String.class, int.class, int.class, profilerInfo, Bundle.class, int.class);
+        }
+        return startActivityAsUserWithFeatureMethod;
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    public int startActivityAsUserWithFeature(Intent intent) {
+        try {
+            Method method = getStartActivityAsUserWithFeatureMethod();
+            return (int) method.invoke(
+                    /* this */ manager,
+                    /* caller */ null,
+                    /* callingPackage */ FakeContext.PACKAGE_NAME,
+                    /* callingFeatureId */ null,
+                    /* intent */ intent,
+                    /* resolvedType */ null,
+                    /* resultTo */ null,
+                    /* resultWho */ null,
+                    /* requestCode */ 0,
+                    /* startFlags */ 0,
+                    /* profilerInfo */ null,
+                    /* bOptions */ null,
+                    /* userId */ /* UserHandle.USER_CURRENT */ -2);
+        } catch (Throwable e) {
+            Ln.e("Could not invoke method", e);
+            return 0;
+        }
+    }
+
+    private Method getForceStopPackageMethod() throws NoSuchMethodException {
+        if (forceStopPackageMethod == null) {
+            forceStopPackageMethod = manager.getClass().getMethod("forceStopPackage", String.class, int.class);
+        }
+        return forceStopPackageMethod;
+    }
+
+    public void forceStopPackage(String packageName) {
+        try {
+            Method method = getForceStopPackageMethod();
+            method.invoke(manager, packageName, /* userId */ /* UserHandle.USER_CURRENT */ -2);
+        } catch (Throwable e) {
+            Ln.e("Could not invoke method", e);
+        }
     }
 }
