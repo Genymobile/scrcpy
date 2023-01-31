7c0ee702617dfd702336570f7dbd5f10e8bebdc4
Romain Vimont <rom@rom1v.com>
2023-01-31 22:32:33 +0100

    Use PACKAGE_NAME from FakeContext
    Remove duplicated constant.

--- server/src/main/java/com/genymobile/scrcpy/wrappers/ServiceManager.java
+++ server/src/main/java/com/genymobile/scrcpy/wrappers/ServiceManager.java
@@ -10,7 +10,6 @@ import java.lang.reflect.Method;
 @SuppressLint("PrivateApi,DiscouragedPrivateApi")
 public final class ServiceManager {
 
-    public static final String PACKAGE_NAME = "com.android.shell";
     public static final int USER_ID = 0;
 
     private static final Method GET_SERVICE_METHOD;
