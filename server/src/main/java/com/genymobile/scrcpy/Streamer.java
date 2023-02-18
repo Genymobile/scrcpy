15556d1f3be9aa0cf98c43205b516e622e9c7856
Romain Vimont <rom@rom1v.com>
2023-02-18 17:35:54 +0100

    Extract OPUS extradata
    For OPUS codec, FFmpeg expects the raw extradata, but MediaCodec wraps
    it in some structure.
    
    Fix the config packet to send only the raw extradata.
    
    PR #3757 <https://github.com/Genymobile/scrcpy/pull/3757>

--- server/src/main/java/com/genymobile/scrcpy/Streamer.java
+++ server/src/main/java/com/genymobile/scrcpy/Streamer.java
@@ -11,6 +11,8 @@ public final class Streamer {
     private static final long PACKET_FLAG_CONFIG = 1L << 63;
     private static final long PACKET_FLAG_KEY_FRAME = 1L << 62;
 
+    private static final long AOPUSHDR = 0x5244485355504F41L; // "AOPUSHDR" in ASCII (little-endian)
+
     private final FileDescriptor fd;
     private final Codec codec;
     private final boolean sendCodecId;
@@ -39,6 +41,10 @@ public final class Streamer {
     }
 
     public void writePacket(ByteBuffer buffer, long pts, boolean config, boolean keyFrame) throws IOException {
+        if (config && codec == AudioCodec.OPUS) {
+            fixOpusConfigPacket(buffer);
+        }
+
         if (sendFrameMeta) {
             writeFrameMeta(fd, buffer.remaining(), pts, config, keyFrame);
         }
@@ -70,5 +76,45 @@ public final class Streamer {
         headerBuffer.putInt(packetSize);
         headerBuffer.flip();
         IO.writeFully(fd, headerBuffer);
+    }
+
+    private static void fixOpusConfigPacket(ByteBuffer buffer) throws IOException {
+        // Here is an example of the config packet received for an OPUS stream:
+        //
+        // 00000000  41 4f 50 55 53 48 44 52  13 00 00 00 00 00 00 00  |AOPUSHDR........|
+        // -------------- BELOW IS THE PART WE MUST PUT AS EXTRADATA  -------------------
+        // 00000010  4f 70 75 73 48 65 61 64  01 01 38 01 80 bb 00 00  |OpusHead..8.....|
+        // 00000020  00 00 00                                          |...             |
+        // ------------------------------------------------------------------------------
+        // 00000020           41 4f 50 55 53  44 4c 59 08 00 00 00 00  |   AOPUSDLY.....|
+        // 00000030  00 00 00 a0 2e 63 00 00  00 00 00 41 4f 50 55 53  |.....c.....AOPUS|
+        // 00000040  50 52 4c 08 00 00 00 00  00 00 00 00 b4 c4 04 00  |PRL.............|
+        // 00000050  00 00 00                                          |...|
+        //
+        // Each "section" is prefixed by a 64-bit ID and a 64-bit length.
+        //
+        // <https://developer.android.com/reference/android/media/MediaCodec#CSD>
+
+        if (buffer.remaining() < 16) {
+            throw new IOException("Not enough data in OPUS config packet");
+        }
+
+        long id = buffer.getLong();
+        if (id != AOPUSHDR) {
+            throw new IOException("OPUS header not found");
+        }
+
+        long sizeLong = buffer.getLong();
+        if (sizeLong < 0 || sizeLong >= 0x7FFFFFFF) {
+            throw new IOException("Invalid block size in OPUS header: " + sizeLong);
+        }
+
+        int size = (int) sizeLong;
+        if (buffer.remaining() < size) {
+            throw new IOException("Not enough data in OPUS header (invalid size: " + size + ")");
+        }
+
+        // Set the buffer to point to the OPUS header slice
+        buffer.limit(buffer.position() + size);
     }
 }
