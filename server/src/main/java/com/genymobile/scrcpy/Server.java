3116e7f8cd63238b4552608807d3e2e93be8e765
Romain Vimont <rom@rom1v.com>
2023-02-19 02:27:39 +0100

    Disable MethodLength checkstyle on createOptions()
    This method will grow as needed to initialize options.

--- server/src/main/java/com/genymobile/scrcpy/Server.java
+++ server/src/main/java/com/genymobile/scrcpy/Server.java
@@ -157,6 +157,7 @@ public final class Server {
         return thread;
     }
 
+    @SuppressWarnings("MethodLength")
     private static Options createOptions(String... args) {
         if (args.length < 1) {
             throw new IllegalArgumentException("Missing client version");
