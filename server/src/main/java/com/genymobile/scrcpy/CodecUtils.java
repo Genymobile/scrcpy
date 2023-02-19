2cddc51b80e8bff9fcb9ca64f0c4028898891049
Romain Vimont <rom@rom1v.com>
2023-02-19 20:03:04 +0100

    Extract unknown encoder error message
    This will allow to reuse the same code for audio encoder selection.
    
    PR #3757 <https://github.com/Genymobile/scrcpy/pull/3757>

--- server/src/main/java/com/genymobile/scrcpy/CodecUtils.java
+++ server/src/main/java/com/genymobile/scrcpy/CodecUtils.java
@@ -25,4 +25,29 @@ public final class CodecUtils {
             format.setString(key, (String) value);
         }
     }
+
+    public static String buildUnknownEncoderMessage(Codec codec, String encoderName) {
+        StringBuilder msg = new StringBuilder("Encoder '").append(encoderName).append("' for ").append(codec.getName()).append(" not found");
+        MediaCodecInfo[] encoders = listEncoders(codec.getMimeType());
+        if (encoders != null && encoders.length > 0) {
+            msg.append("\nTry to use one of the available encoders:");
+            String codecOption = codec.getType() == Codec.Type.VIDEO ? "video-codec" : "audio-codec";
+            for (MediaCodecInfo encoder : encoders) {
+                msg.append("\n    scrcpy --").append(codecOption).append("=").append(codec.getName());
+                msg.append(" --encoder='").append(encoder.getName()).append("'");
+            }
+        }
+        return msg.toString();
+    }
+
+    private static MediaCodecInfo[] listEncoders(String mimeType) {
+        List<MediaCodecInfo> result = new ArrayList<>();
+        MediaCodecList list = new MediaCodecList(MediaCodecList.REGULAR_CODECS);
+        for (MediaCodecInfo codecInfo : list.getCodecInfos()) {
+            if (codecInfo.isEncoder() && Arrays.asList(codecInfo.getSupportedTypes()).contains(mimeType)) {
+                result.add(codecInfo);
+            }
+        }
+        return result.toArray(new MediaCodecInfo[result.size()]);
+    }
 }
