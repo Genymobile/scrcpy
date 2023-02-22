7ba2da9662770472508fe84c8bf1b4bee4b498a9
Romain Vimont <rom@rom1v.com>
2023-02-22 22:44:01 +0100

    Rename --encoder to --video-encoder
    This prepares the introduction of --audio-encoder.

--- app/src/server.c
+++ app/src/server.c
@@ -72,7 +72,7 @@ sc_server_params_destroy(struct sc_server_params *params) {
     free((char *) params->req_serial);
     free((char *) params->crop);
     free((char *) params->video_codec_options);
-    free((char *) params->encoder_name);
+    free((char *) params->video_encoder);
     free((char *) params->tcpip_dst);
 }
 
@@ -96,7 +96,7 @@ sc_server_params_copy(struct sc_server_params *dst,
     COPY(req_serial);
     COPY(crop);
     COPY(video_codec_options);
-    COPY(encoder_name);
+    COPY(video_encoder);
     COPY(tcpip_dst);
 #undef COPY
 
@@ -258,8 +258,8 @@ execute_server(struct sc_server *server,
     if (params->video_codec_options) {
         ADD_PARAM("video_codec_options=%s", params->video_codec_options);
     }
-    if (params->encoder_name) {
-        ADD_PARAM("encoder_name=%s", params->encoder_name);
+    if (params->video_encoder) {
+        ADD_PARAM("video_encoder=%s", params->video_encoder);
     }
     if (params->power_off_on_close) {
         ADD_PARAM("power_off_on_close=true");
