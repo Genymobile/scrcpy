6e7c4a1b4029af1ed0f0ee66d67406321180e26a
Romain Vimont <rom@rom1v.com>
2023-02-23 23:12:24 +0100

    Log display sizes in display list
    This is more convenient than just the display id alone.

--- server/src/main/java/com/genymobile/scrcpy/LogUtils.java
+++ server/src/main/java/com/genymobile/scrcpy/LogUtils.java
@@ -1,5 +1,6 @@
 package com.genymobile.scrcpy;
 
+import com.genymobile.scrcpy.wrappers.DisplayManager;
 import com.genymobile.scrcpy.wrappers.ServiceManager;
 
 import java.util.List;
@@ -40,12 +41,21 @@ public final class LogUtils {
 
     public static String buildDisplayListMessage() {
         StringBuilder builder = new StringBuilder("List of displays:");
-        int[] displayIds = ServiceManager.getDisplayManager().getDisplayIds();
+        DisplayManager displayManager = ServiceManager.getDisplayManager();
+        int[] displayIds = displayManager.getDisplayIds();
         if (displayIds == null || displayIds.length == 0) {
             builder.append("\n    (none)");
         } else {
             for (int id : displayIds) {
-                builder.append("\n    --display=").append(id);
+                builder.append("\n    --display=").append(id).append("    (");
+                DisplayInfo displayInfo = displayManager.getDisplayInfo(id);
+                if (displayInfo != null) {
+                    Size size = displayInfo.getSize();
+                    builder.append(size.getWidth()).append("x").append(size.getHeight());
+                } else {
+                    builder.append("size unknown");
+                }
+                builder.append(")");
             }
         }
         return builder.toString();
