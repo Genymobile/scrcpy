954c77489461c658ebba4b38145490fc7a98d744
Romain Vimont <rom@rom1v.com>
2023-02-23 23:10:15 +0100

    Add --list-device-displays

--- app/src/scrcpy.c
+++ app/src/scrcpy.c
@@ -353,6 +353,7 @@ scrcpy(struct scrcpy_options *options) {
         .cleanup = options->cleanup,
         .power_on = options->power_on,
         .list_encoders = options->list_encoders,
+        .list_displays = options->list_displays,
     };
 
     static const struct sc_server_callbacks cbs = {
@@ -370,7 +371,7 @@ scrcpy(struct scrcpy_options *options) {
 
     server_started = true;
 
-    if (options->list_encoders) {
+    if (options->list_encoders || options->list_displays) {
         bool ok = await_for_server(NULL);
         ret = ok ? SCRCPY_EXIT_SUCCESS : SCRCPY_EXIT_FAILURE;
         goto end;
