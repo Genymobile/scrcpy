3c18cfb23bfc896ddeb51d9829e09ff68bc9ffc9
Romain Vimont <rom@rom1v.com>
2023-02-25 18:45:05 +0100

    Add two-step write feature to bytebuf
    If there is exactly one producer, then it can assume that the remaining
    space in the buffer will only increase until it write something.
    
    This assumption may allow the producer to write to the buffer (up to a
    known safe size) without any synchronization mechanism, thus allowing
    to read and write different parts of the buffer in parallel.
    
    The producer can then commit the write with lock held, and update its
    knowledge of the safe empty remaining space.

--- app/tests/test_bytebuf.c
+++ app/tests/test_bytebuf.c
@@ -71,12 +71,56 @@ void test_bytebuf_boundaries(void) {
     sc_bytebuf_destroy(&buf);
 }
 
+void test_bytebuf_two_steps_write(void) {
+    struct sc_bytebuf buf;
+    uint8_t data[20];
+
+    bool ok = sc_bytebuf_init(&buf, 20);
+    assert(ok);
+
+    sc_bytebuf_write(&buf, (uint8_t *) "hello ", sizeof("hello ") - 1);
+    assert(sc_bytebuf_read_available(&buf) == 6);
+
+    sc_bytebuf_write(&buf, (uint8_t *) "hello ", sizeof("hello ") - 1);
+    assert(sc_bytebuf_read_available(&buf) == 12);
+
+    sc_bytebuf_prepare_write(&buf, (uint8_t *) "hello ", sizeof("hello ") - 1);
+    assert(sc_bytebuf_read_available(&buf) == 12); // write not committed yet
+
+    sc_bytebuf_read(&buf, data, 9);
+    assert(!strncmp((char *) data, "hello hel", 3));
+    assert(sc_bytebuf_read_available(&buf) == 3);
+
+    sc_bytebuf_commit_write(&buf, sizeof("hello ") - 1);
+    assert(sc_bytebuf_read_available(&buf) == 9);
+
+    sc_bytebuf_prepare_write(&buf, (uint8_t *) "world", sizeof("world") - 1);
+    assert(sc_bytebuf_read_available(&buf) == 9); // write not committed yet
+
+    sc_bytebuf_commit_write(&buf, sizeof("world") - 1);
+    assert(sc_bytebuf_read_available(&buf) == 14);
+
+    sc_bytebuf_write(&buf, (uint8_t *) "!", 1);
+    assert(sc_bytebuf_read_available(&buf) == 15);
+
+    sc_bytebuf_skip(&buf, 3);
+    assert(sc_bytebuf_read_available(&buf) == 12);
+
+    sc_bytebuf_read(&buf, data, 12);
+    data[12] = '\0';
+    assert(!strcmp((char *) data, "hello world!"));
+    assert(sc_bytebuf_read_available(&buf) == 0);
+
+    sc_bytebuf_destroy(&buf);
+}
+
 int main(int argc, char *argv[]) {
     (void) argc;
     (void) argv;
 
     test_bytebuf_simple();
     test_bytebuf_boundaries();
+    test_bytebuf_two_steps_write();
 
     return 0;
 }
