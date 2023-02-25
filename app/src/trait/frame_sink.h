ffe3b87f3cf000c3fcd703f13e0bb26e40a762c6
Romain Vimont <rom@rom1v.com>
2023-02-25 16:19:58 +0100

    Pass AVCodecContext to frame sinks
    Frame consumers may need details about the frame format.

--- app/src/trait/frame_sink.h
+++ app/src/trait/frame_sink.h
@@ -5,6 +5,7 @@
 
 #include <assert.h>
 #include <stdbool.h>
+#include <libavcodec/avcodec.h>
 
 typedef struct AVFrame AVFrame;
 
@@ -18,7 +19,7 @@ struct sc_frame_sink {
 };
 
 struct sc_frame_sink_ops {
-    bool (*open)(struct sc_frame_sink *sink);
+    bool (*open)(struct sc_frame_sink *sink, const AVCodecContext *ctx);
     void (*close)(struct sc_frame_sink *sink);
     bool (*push)(struct sc_frame_sink *sink, const AVFrame *frame);
 };
