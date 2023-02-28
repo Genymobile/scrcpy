389dd77b5060af9989527729bad5f72c0818d2e6
Romain Vimont <rom@rom1v.com>
2023-02-28 23:52:26 +0100

    Fix MIN/MAX macros
    Expressions like "x < MAX(y, z)" were broken.

--- app/src/common.h
+++ app/src/common.h
@@ -5,8 +5,8 @@
 #include "compat.h"
 
 #define ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
-#define MIN(X,Y) (X) < (Y) ? (X) : (Y)
-#define MAX(X,Y) (X) > (Y) ? (X) : (Y)
+#define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
+#define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
 #define CLAMP(V,X,Y) MIN( MAX((V),(X)), (Y) )
 
 #define container_of(ptr, type, member) \
