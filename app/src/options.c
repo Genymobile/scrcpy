cdd0cdbcebba47d9cf0c4460902999b39adf59fe
Romain Vimont <rom@rom1v.com>
2023-03-02 23:14:01 +0100

    Add --audio-buffer
    Expose an option to add a buffering delay (in milliseconds) before
    playing audio.
    
    This is similar to the options --display-buffer and --v4l2-buffer for
    video frames.
    
    PR #3757 <https://github.com/Genymobile/scrcpy/pull/3757>

--- app/src/options.c
+++ app/src/options.c
@@ -43,6 +43,7 @@ const struct scrcpy_options scrcpy_options_default = {
     .display_id = 0,
     .display_buffer = 0,
     .v4l2_buffer = 0,
+    .audio_buffer = SC_TICK_FROM_MS(50),
 #ifdef HAVE_USB
     .otg = false,
 #endif
