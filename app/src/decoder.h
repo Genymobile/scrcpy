9b19de4262b600ef3470567a1b5e45d08314ff91
Romain Vimont <rom@rom1v.com>
2023-03-02 09:37:36 +0100

    Use frame source trait in decoder

--- app/src/decoder.h
+++ app/src/decoder.h
@@ -3,21 +3,18 @@
 
 #include "common.h"
 
+#include "trait/frame_source.h"
 #include "trait/packet_sink.h"
 
 #include <stdbool.h>
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 
-#define SC_DECODER_MAX_SINKS 2
-
 struct sc_decoder {
     struct sc_packet_sink packet_sink; // packet sink trait
+    struct sc_frame_source frame_source; // frame source trait
 
     const char *name; // must be statically allocated (e.g. a string literal)
-
-    struct sc_frame_sink *sinks[SC_DECODER_MAX_SINKS];
-    unsigned sink_count;
 
     AVCodecContext *codec_ctx;
     AVFrame *frame;
@@ -26,8 +23,5 @@ struct sc_decoder {
 // The name must be statically allocated (e.g. a string literal)
 void
 sc_decoder_init(struct sc_decoder *decoder, const char *name);
-
-void
-sc_decoder_add_sink(struct sc_decoder *decoder, struct sc_frame_sink *sink);
 
 #endif
