d046678f85c0730570e807a2d25267280beab086
Romain Vimont <rom@rom1v.com>
2023-06-22 00:10:37 +0200

    Upgrade platform-tools (34.0.3) for Windows
    Include the latest version of adb in Windows releases.

--- app/prebuilt-deps/prepare-adb.sh
+++ app/prebuilt-deps/prepare-adb.sh
@@ -6,10 +6,10 @@ cd "$DIR"
 mkdir -p "$PREBUILT_DATA_DIR"
 cd "$PREBUILT_DATA_DIR"
 
-DEP_DIR=platform-tools-34.0.1
+DEP_DIR=platform-tools-34.0.3
 
-FILENAME=platform-tools_r34.0.1-windows.zip
-SHA256SUM=5dd9c2be744c224fa3a7cbe30ba02d2cb378c763bd0f797a7e47e9f3156a5daa
+FILENAME=platform-tools_r34.0.3-windows.zip
+SHA256SUM=fce992e93eb786fc9f47df93d83a7b912c46742d45c39d712c02e06d05b72e2b
 
 if [[ -d "$DEP_DIR" ]]
 then
