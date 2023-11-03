be50f85adbc14c6d88fae62f4c9dc7690d4af208
Romain Vimont <rom@rom1v.com>
2023-11-03 15:53:25 +0100

    Attempt to fix 4392

--- server/src/main/java/com/genymobile/scrcpy/FakeContext.java
+++ server/src/main/java/com/genymobile/scrcpy/FakeContext.java
@@ -2,11 +2,11 @@ package com.genymobile.scrcpy;
 
 import android.annotation.TargetApi;
 import android.content.AttributionSource;
-import android.content.MutableContextWrapper;
+import android.content.ContextWrapper;
 import android.os.Build;
 import android.os.Process;
 
-public final class FakeContext extends MutableContextWrapper {
+public final class FakeContext extends ContextWrapper {
 
     public static final String PACKAGE_NAME = "com.android.shell";
     public static final int ROOT_UID = 0; // Like android.os.Process.ROOT_UID, but before API 29
@@ -18,7 +18,7 @@ public final class FakeContext extends MutableContextWrapper {
     }
 
     private FakeContext() {
-        super(null);
+        super(Workarounds.getSystemContext());
     }
 
     @Override
