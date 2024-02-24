b51da9a278d32bcb3a3ef31fa2ac46097ff59591
Romain Vimont <rom@rom1v.com>
2024-02-24 22:30:30 +0100

    Add shortcut to open keyboard settings
    The keyboard settings can be opened by:
    
        adb shell am start -a android.settings.HARD_KEYBOARD_SETTINGS
    
    Add a shortcut (MOD+k) for convenience if the current keyboard is HID.

--- server/src/main/java/com/genymobile/scrcpy/Controller.java
+++ server/src/main/java/com/genymobile/scrcpy/Controller.java
@@ -1,7 +1,9 @@
 package com.genymobile.scrcpy;
 
 import com.genymobile.scrcpy.wrappers.InputManager;
+import com.genymobile.scrcpy.wrappers.ServiceManager;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.SystemClock;
 import android.view.InputDevice;
@@ -207,6 +209,9 @@ public class Controller implements AsyncProcessor {
                 break;
             case ControlMessage.TYPE_UHID_INPUT:
                 getUhidManager().writeInput(msg.getId(), msg.getData());
+                break;
+            case ControlMessage.TYPE_OPEN_HARD_KEYBOARD_SETTINGS:
+                openHardKeyboardSettings();
                 break;
             default:
                 // do nothing
@@ -445,5 +450,10 @@ public class Controller implements AsyncProcessor {
         }
 
         return ok;
+    }
+
+    private void openHardKeyboardSettings() {
+        Intent intent = new Intent("android.settings.HARD_KEYBOARD_SETTINGS");
+        ServiceManager.getActivityManager().startActivity(intent);
     }
 }
