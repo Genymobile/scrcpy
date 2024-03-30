be3d357a6dcdf0f3fccabd795b869a30e1e5bee1
Romain Vimont <rom@rom1v.com>
2024-03-30 11:23:41 +0100

    Use source repo tarball for libusb
    Legitimate or not, we should not use sources that do not match the
    repository.
    
    Refs <https://github.com/libusb/libusb/issues/1468#issuecomment-1974787595>
    Refs <https://news.ycombinator.com/item?id=39866309>
    Refs #4713 <https://github.com/Genymobile/scrcpy/pull/4713>

--- app/deps/libusb.sh
+++ app/deps/libusb.sh
@@ -5,9 +5,9 @@ cd "$DEPS_DIR"
 . common
 
 VERSION=1.0.27
-FILENAME=libusb-$VERSION.tar.bz2
+FILENAME=libusb-$VERSION.tar.gz
 PROJECT_DIR=libusb-$VERSION
-SHA256SUM=ffaa41d741a8a3bee244ac8e54a72ea05bf2879663c098c82fc5757853441575
+SHA256SUM=e8f18a7a36ecbb11fb820bd71540350d8f61bcd9db0d2e8c18a6fb80b214a3de
 
 cd "$SOURCES_DIR"
 
@@ -15,7 +15,7 @@ if [[ -d "$PROJECT_DIR" ]]
 then
     echo "$PWD/$PROJECT_DIR" found
 else
-    get_file "https://github.com/libusb/libusb/releases/download/v$VERSION/libusb-$VERSION.tar.bz2" "$FILENAME" "$SHA256SUM"
+    get_file "https://github.com/libusb/libusb/archive/refs/tags/v$VERSION.tar.gz" "$FILENAME" "$SHA256SUM"
     tar xf "$FILENAME"  # First level directory is "$PROJECT_DIR"
 fi
 
@@ -33,6 +33,7 @@ else
     mkdir "$HOST"
     cd "$HOST"
 
+    "$SOURCES_DIR/$PROJECT_DIR"/bootstrap.sh
     "$SOURCES_DIR/$PROJECT_DIR"/configure \
         --prefix="$INSTALL_DIR/$HOST" \
         --host="$HOST_TRIPLET" \
