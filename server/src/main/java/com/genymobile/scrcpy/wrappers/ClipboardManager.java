397b51784c9fc3c29210e5d1ce5906201ae3ee25
Kaiming Hu <huxxx1234@gmail.com>
2024-04-07 09:20:24 +0800

    Fix copy-paste for some devices

--- server/src/main/java/com/genymobile/scrcpy/wrappers/ClipboardManager.java
+++ server/src/main/java/com/genymobile/scrcpy/wrappers/ClipboardManager.java
@@ -60,10 +60,16 @@ public final class ClipboardManager {
                                             .getMethod("getPrimaryClip", String.class, String.class, int.class, int.class, boolean.class);
                                     getMethodVersion = 4;
                                 } catch (NoSuchMethodException e5) {
-                                    getPrimaryClipMethod = manager.getClass()
-                                            .getMethod("getPrimaryClip", String.class, String.class, String.class, String.class, int.class, int.class,
-                                                    boolean.class);
-                                    getMethodVersion = 5;
+                                    try {
+                                        getPrimaryClipMethod = manager.getClass()
+                                                .getMethod("getPrimaryClip", String.class, String.class, String.class, String.class, int.class, int.class,
+                                                        boolean.class);
+                                        getMethodVersion = 5;
+                                    } catch (NoSuchMethodException e6) {
+                                        getPrimaryClipMethod = manager.getClass()
+                                                .getMethod("getPrimaryClip", String.class, String.class, int.class, int.class, String.class);
+                                        getMethodVersion = 6;
+                                    }
                                 }
                             }
                         }
@@ -120,8 +126,10 @@ public final class ClipboardManager {
             case 4:
                 // The last boolean parameter is "userOperate"
                 return (ClipData) method.invoke(manager, FakeContext.PACKAGE_NAME, null, FakeContext.ROOT_UID, 0, true);
-            default:
+            case 5:
                 return (ClipData) method.invoke(manager, FakeContext.PACKAGE_NAME, null, null, null, FakeContext.ROOT_UID, 0, true);
+            default:
+                return (ClipData) method.invoke(manager, FakeContext.PACKAGE_NAME, null, FakeContext.ROOT_UID, 0, null);
         }
     }
 
