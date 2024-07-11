79242957a0ddba1c326f414cf5eafff6cc7b39c1
Romain Vimont <rom@rom1v.com>
2024-07-11 12:21:38 +0200

    Add clipboard workaround for Honor device
    Fixes #5073 <https://github.com/Genymobile/scrcpy/issues/5073>

--- server/src/main/java/com/genymobile/scrcpy/wrappers/ClipboardManager.java
+++ server/src/main/java/com/genymobile/scrcpy/wrappers/ClipboardManager.java
@@ -82,9 +82,17 @@ public final class ClipboardManager {
                 // fall-through
             }
 
-            getPrimaryClipMethod = manager.getClass()
-                    .getMethod("getPrimaryClip", String.class, String.class, String.class, String.class, int.class, int.class, boolean.class);
-            getMethodVersion = 5;
+            try {
+                getPrimaryClipMethod = manager.getClass()
+                        .getMethod("getPrimaryClip", String.class, String.class, String.class, String.class, int.class, int.class, boolean.class);
+                getMethodVersion = 5;
+                return getPrimaryClipMethod;
+            } catch (NoSuchMethodException e) {
+                // fall-through
+            }
+
+            getPrimaryClipMethod = manager.getClass().getMethod("getPrimaryClip", String.class, String.class, int.class, int.class, String.class);
+            getMethodVersion = 6;
         }
         return getPrimaryClipMethod;
     }
@@ -145,8 +153,10 @@ public final class ClipboardManager {
             case 4:
                 // The last boolean parameter is "userOperate"
                 return (ClipData) method.invoke(manager, FakeContext.PACKAGE_NAME, null, FakeContext.ROOT_UID, 0, true);
-            default:
+            case 5:
                 return (ClipData) method.invoke(manager, FakeContext.PACKAGE_NAME, null, null, null, FakeContext.ROOT_UID, 0, true);
+            default:
+                return (ClipData) method.invoke(manager, FakeContext.PACKAGE_NAME, null, FakeContext.ROOT_UID, 0, null);
         }
     }
 
