24f7ea5894f7aaa09aedaa05119330e98b256aac
Romain Vimont <rom@rom1v.com>
2024-09-06 23:08:08 +0200

    Rename hid_event to hid_input
    The sc_hid_event structure represents HID input data. Rename it so that
    we can add other hid event structs without confusion.

--- app/src/hid/hid_mouse.h
+++ app/src/hid/hid_mouse.h
@@ -14,15 +14,15 @@ extern const uint8_t SC_HID_MOUSE_REPORT_DESC[];
 extern const size_t SC_HID_MOUSE_REPORT_DESC_LEN;
 
 void
-sc_hid_mouse_event_from_motion(struct sc_hid_event *hid_event,
-                               const struct sc_mouse_motion_event *event);
+sc_hid_mouse_generate_input_from_motion(struct sc_hid_input *hid_input,
+                                    const struct sc_mouse_motion_event *event);
 
 void
-sc_hid_mouse_event_from_click(struct sc_hid_event *hid_event,
-                              const struct sc_mouse_click_event *event);
+sc_hid_mouse_generate_input_from_click(struct sc_hid_input *hid_input,
+                                    const struct sc_mouse_click_event *event);
 
 void
-sc_hid_mouse_event_from_scroll(struct sc_hid_event *hid_event,
-                               const struct sc_mouse_scroll_event *event);
+sc_hid_mouse_generate_input_from_scroll(struct sc_hid_input *hid_input,
+                                    const struct sc_mouse_scroll_event *event);
 
 #endif
