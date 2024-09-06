4565f36ee6993eff3b0376ad2da74a71060bc5b0
Romain Vimont <rom@rom1v.com>
2024-09-06 23:08:08 +0200

    Handle SDL gamepad events
    Introduce a gamepad processor trait, similar to the keyboard processor
    and mouse processor traits.
    
    Handle gamepad events received from SDL, convert them to scrcpy-specific
    gamepad events, and forward them to the gamepad processor.
    
    Further commits will provide AOA and UHID implementations of the gamepad
    processor trait.
    
    PR #5270 <https://github.com/Genymobile/scrcpy/pull/5270>
    
    Co-authored-by: Luiz Henrique Laurini <luizhenriquelaurini@gmail.com>

--- app/src/scrcpy.c
+++ app/src/scrcpy.c
@@ -485,6 +485,11 @@ scrcpy(struct scrcpy_options *options) {
         }
     }
 
+    if (SDL_Init(SDL_INIT_GAMECONTROLLER)) {
+        LOGE("Could not initialize SDL gamepad: %s", SDL_GetError());
+        goto end;
+    }
+
     sdl_configure(options->video_playback, options->disable_screensaver);
 
     // Await for server without blocking Ctrl+C handling
@@ -735,6 +740,7 @@ aoa_complete:
             .fp = fp,
             .kp = kp,
             .mp = mp,
+            .gp = NULL,
             .mouse_bindings = options->mouse_bindings,
             .legacy_paste = options->legacy_paste,
             .clipboard_autosync = options->clipboard_autosync,
---
+++ app/src/trait/gamepad_processor.h
@@ -0,0 +1,50 @@
+#ifndef SC_GAMEPAD_PROCESSOR_H
+#define SC_GAMEPAD_PROCESSOR_H
+
+#include "common.h"
+
+#include <assert.h>
+#include <stdbool.h>
+
+#include "input_events.h"
+
+/**
+ * Gamepad processor trait.
+ *
+ * Component able to handle gamepads devices and inject buttons and axis events.
+ */
+struct sc_gamepad_processor {
+    const struct sc_gamepad_processor_ops *ops;
+};
+
+struct sc_gamepad_processor_ops {
+
+    /**
+     * Process a gamepad device added or removed
+     *
+     * This function is mandatory.
+     */
+    void
+    (*process_gamepad_device)(struct sc_gamepad_processor *gp,
+                              const struct sc_gamepad_device_event *event);
+
+    /**
+     * Process a gamepad axis event
+     *
+     * This function is mandatory.
+     */
+    void
+    (*process_gamepad_axis)(struct sc_gamepad_processor *gp,
+                            const struct sc_gamepad_axis_event *event);
+
+    /**
+     * Process a gamepad button event
+     *
+     * This function is mandatory.
+     */
+    void
+    (*process_gamepad_button)(struct sc_gamepad_processor *gp,
+                              const struct sc_gamepad_button_event *event);
+};
+
+#endif
