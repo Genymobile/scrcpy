ca08d45bd7067d07d6246a8c6e99d97f35b9e548
Romain Vimont <rom@rom1v.com>
2024-09-06 23:08:08 +0200

    Extract sc_push_event()
    Expose a convenience function to push an event without args to the main
    thread.
    
    PR #5270 <https://github.com/Genymobile/scrcpy/pull/5270>

--- app/src/screen.c
+++ app/src/screen.c
@@ -312,14 +312,9 @@ sc_screen_frame_sink_open(struct sc_frame_sink *sink,
     screen->frame_size.width = ctx->width;
     screen->frame_size.height = ctx->height;
 
-    static SDL_Event event = {
-        .type = SC_EVENT_SCREEN_INIT_SIZE,
-    };
-
     // Post the event on the UI thread (the texture must be created from there)
-    int ret = SDL_PushEvent(&event);
-    if (ret < 0) {
-        LOGW("Could not post init size event: %s", SDL_GetError());
+    bool ok = sc_push_event(SC_EVENT_SCREEN_INIT_SIZE);
+    if (!ok) {
         return false;
     }
 
@@ -358,14 +353,9 @@ sc_screen_frame_sink_push(struct sc_frame_sink *sink, const AVFrame *frame) {
         // The SC_EVENT_NEW_FRAME triggered for the previous frame will consume
         // this new frame instead
     } else {
-        static SDL_Event new_frame_event = {
-            .type = SC_EVENT_NEW_FRAME,
-        };
-
         // Post the event on the UI thread
-        int ret = SDL_PushEvent(&new_frame_event);
-        if (ret < 0) {
-            LOGW("Could not post new frame event: %s", SDL_GetError());
+        bool ok = sc_push_event(SC_EVENT_NEW_FRAME);
+        if (!ok) {
             return false;
         }
     }
