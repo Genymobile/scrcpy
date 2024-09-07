21b412cd9879b850e44fc0cf52012e8f8078bc60
Romain Vimont <rom@rom1v.com>
2024-09-07 14:24:25 +0200

    Simplify messages reader/writer
    In Java, control messages were parsed using manual buffering, which was
    convoluted and error-prone.
    
    Instead, read the socket directly through a DataInputStream and a
    BufferedInputStream. Symmetrically, use a DataOutputStream and a
    BufferedOutputStream to write messages.

--- server/src/main/java/com/genymobile/scrcpy/control/ControlChannel.java
+++ server/src/main/java/com/genymobile/scrcpy/control/ControlChannel.java
@@ -3,31 +3,22 @@ package com.genymobile.scrcpy.control;
 import android.net.LocalSocket;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public final class ControlChannel {
-    private final InputStream inputStream;
-    private final OutputStream outputStream;
 
-    private final ControlMessageReader reader = new ControlMessageReader();
-    private final DeviceMessageWriter writer = new DeviceMessageWriter();
+    private final ControlMessageReader reader;
+    private final DeviceMessageWriter writer;
 
     public ControlChannel(LocalSocket controlSocket) throws IOException {
-        this.inputStream = controlSocket.getInputStream();
-        this.outputStream = controlSocket.getOutputStream();
+        reader = new ControlMessageReader(controlSocket.getInputStream());
+        writer = new DeviceMessageWriter(controlSocket.getOutputStream());
     }
 
     public ControlMessage recv() throws IOException {
-        ControlMessage msg = reader.next();
-        while (msg == null) {
-            reader.readFrom(inputStream);
-            msg = reader.next();
-        }
-        return msg;
+        return reader.read();
     }
 
     public void send(DeviceMessage msg) throws IOException {
-        writer.writeTo(msg, outputStream);
+        writer.write(msg);
     }
 }
