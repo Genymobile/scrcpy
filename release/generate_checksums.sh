a7efb180b9befa576513c31e2db0340d0ee33aeb
Romain Vimont <rom@rom1v.com>
2024-11-22 22:23:27 +0100

    Add script to release macOS static binary
    Provide a prebuilt binary for macOS.
    
    Fixes #1733 <https://github.com/Genymobile/scrcpy/issues/1733>
    Fixes #3235 <https://github.com/Genymobile/scrcpy/issues/3235>
    Fixes #4489 <https://github.com/Genymobile/scrcpy/issues/4489>
    Fixes #5327 <https://github.com/Genymobile/scrcpy/issues/5327>
    PR #5515 <https://github.com/Genymobile/scrcpy/pull/5515>
    
    Co-authored-by: Muvaffak Onus <me@muvaf.com>

--- release/generate_checksums.sh
+++ release/generate_checksums.sh
@@ -8,5 +8,6 @@ sha256sum "scrcpy-server-$VERSION" \
     "scrcpy-linux-$VERSION.tar.gz" \
     "scrcpy-win32-$VERSION.zip" \
     "scrcpy-win64-$VERSION.zip" \
+    "scrcpy-macos-$VERSION.tar.gz" \
         | tee SHA256SUMS.txt
 echo "Release checksums generated in $PWD/SHA256SUMS.txt"
