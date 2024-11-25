243ce920f2ce5c1252cc09885744c598df02f822
Genxster <ck.2229.ck@gmail.com>
2024-11-25 20:26:04 +0530

    Update generate_checksums.sh

--- release/generate_checksums.sh
+++ release/generate_checksums.sh
@@ -8,6 +8,7 @@ sha256sum "scrcpy-server-$VERSION" \
     "scrcpy-linux-$VERSION.tar.gz" \
     "scrcpy-win32-$VERSION.zip" \
     "scrcpy-win64-$VERSION.zip" \
-    "scrcpy-macos-$VERSION.tar.gz" \
+    "scrcpy-macos-intel-$VERSION.tar.gz" \
+    "scrcpy-macos-aarch64-$VERSION.tar.gz" \
         | tee SHA256SUMS.txt
 echo "Release checksums generated in $PWD/SHA256SUMS.txt"
