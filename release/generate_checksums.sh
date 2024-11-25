6a2f35e7dc12f063fd1df9603a363161e882b011
Romain Vimont <rom@rom1v.com>
2024-11-25 10:28:39 +0100

    Specify architecture for Linux and macOS releases
    Co-authored-by: Genxster1998 <ck.2229.ck@gmail.com>

--- release/generate_checksums.sh
+++ release/generate_checksums.sh
@@ -5,9 +5,9 @@ cd "$(dirname ${BASH_SOURCE[0]})"
 
 cd "$OUTPUT_DIR"
 sha256sum "scrcpy-server-$VERSION" \
-    "scrcpy-linux-$VERSION.tar.gz" \
+    "scrcpy-linux-x86_64-$VERSION.tar.gz" \
     "scrcpy-win32-$VERSION.zip" \
     "scrcpy-win64-$VERSION.zip" \
-    "scrcpy-macos-$VERSION.tar.gz" \
+    "scrcpy-macos-aarch64-$VERSION.tar.gz" \
         | tee SHA256SUMS.txt
 echo "Release checksums generated in $PWD/SHA256SUMS.txt"
