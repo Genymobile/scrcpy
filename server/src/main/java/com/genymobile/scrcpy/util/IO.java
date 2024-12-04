Genymobile/scrcpy

commit 9555d3a537a828731ad89ef5258ba537acf8cc11

Retry capture on IOException
If the capture fails with an IOException, retry with a lower resolution.

Fixes #5539 <https://github.com/Genymobile/scrcpy/issues/5539>

server/src/main/java/com/genymobile/scrcpy/video/SurfaceEncoder.java
@@ -112,8 +112,8 @@ public class SurfaceEncoder implements AsyncProcessor {
                         // The capture might have been closed internally (for example if the camera is disconnected)
                         alive = !stopped.get() && !capture.isClosed();
                     }
-                } catch (IllegalStateException | IllegalArgumentException e) {
-                    Ln.e("Encoding error: " + e.getClass().getName() + ": " + e.getMessage());
+                } catch (IllegalStateException | IllegalArgumentException | IOException e) {
+                    Ln.e("Capture/encoding error: " + e.getClass().getName() + ": " + e.getMessage());
                     if (!prepareRetry(size)) {
                         throw e;
                     }

commit 5c3626ed47b983625481f852073cef859e7fcaf9

Handle broken pipe errors specifically
Since 9555d3a537a828731ad89ef5258ba537acf8cc11, a capture/encoding error
was sometimes logged on exit.

server/src/main/java/com/genymobile/scrcpy/util/IO.java
@@ -72,4 +72,8 @@ public final class IO {
         Throwable cause = e.getCause();
         return cause instanceof ErrnoException && ((ErrnoException) cause).errno == OsConstants.EPIPE;
     }
+
+    public static boolean isBrokenPipe(Exception e) {
+        return e instanceof IOException && isBrokenPipe((IOException) e);
+    }
 }

server/src/main/java/com/genymobile/scrcpy/video/SurfaceEncoder.java
@@ -113,6 +113,10 @@ public class SurfaceEncoder implements AsyncProcessor {
                         alive = !stopped.get() && !capture.isClosed();
                     }
                 } catch (IllegalStateException | IllegalArgumentException | IOException e) {
+                    if (IO.isBrokenPipe(e)) {
+                        // Do not retry on broken pipe, which is expected on close because the socket is closed by the client
+                        throw e;
+                    }
                     Ln.e("Capture/encoding error: " + e.getClass().getName() + ": " + e.getMessage());
                     if (!prepareRetry(size)) {
                         throw e;