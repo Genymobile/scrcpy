9fb7446b88a10d68ed2cfeaf411574300ac02455
Romain Vimont <rom@rom1v.com>
2025-02-22 11:40:24 +0100

    Refactor audio sources
    Store the target audio source integer (one of the constants in
    android.media.MediaRecorder.AudioSource) in the AudioSource enum (or -1
    if not relevant).
    
    This will simplify adding new audio sources.

--- server/src/main/java/com/genymobile/scrcpy/audio/AudioSource.java
+++ server/src/main/java/com/genymobile/scrcpy/audio/AudioSource.java
@@ -1,18 +1,26 @@
 package com.genymobile.scrcpy.audio;
 
+import android.media.MediaRecorder;
+
 public enum AudioSource {
-    OUTPUT("output"),
-    MIC("mic"),
-    PLAYBACK("playback");
+    OUTPUT("output", MediaRecorder.AudioSource.REMOTE_SUBMIX),
+    MIC("mic", MediaRecorder.AudioSource.MIC),
+    PLAYBACK("playback", -1);
 
     private final String name;
+    private final int directAudioSource;
 
-    AudioSource(String name) {
+    AudioSource(String name, int directAudioSource) {
         this.name = name;
+        this.directAudioSource = directAudioSource;
     }
 
     public boolean isDirect() {
         return this != PLAYBACK;
+    }
+
+    public int getDirectAudioSource() {
+        return directAudioSource;
     }
 
     public static AudioSource findByName(String name) {
