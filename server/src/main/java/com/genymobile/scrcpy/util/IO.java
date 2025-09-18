4fe73fda248b77801d82a10a75deaa64308d9ea4
Eugen Pechanec <e.pechanec@gmail.com>
2025-09-18 16:18:14 +0200

    Make IO compatible with API 19

--- server/src/main/java/com/genymobile/scrcpy/util/IO.java
+++ server/src/main/java/com/genymobile/scrcpy/util/IO.java
@@ -8,10 +8,14 @@ import android.system.ErrnoException;
 import android.system.Os;
 import android.system.OsConstants;
 
+import androidx.annotation.RequiresApi;
+
 import java.io.FileDescriptor;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Scanner;
 
 public final class IO {
@@ -19,31 +23,38 @@ public final class IO {
         // not instantiable
     }
 
-    private static int write(FileDescriptor fd, ByteBuffer from) throws IOException {
-        while (true) {
-            try {
-                return Os.write(fd, from);
-            } catch (ErrnoException e) {
-                if (e.errno != OsConstants.EINTR) {
-                    throw new IOException(e);
+    @RequiresApi(AndroidVersions.API_21_ANDROID_5_0)
+    private static class Api21 {
+
+        static int write(FileDescriptor fd, ByteBuffer from) throws IOException {
+            while (true) {
+                try {
+                    return Os.write(fd, from);
+                } catch (ErrnoException e) {
+                    if (e.errno != OsConstants.EINTR) {
+                        throw new IOException(e);
+                    }
                 }
             }
+        }
+
+        private Api21() {
         }
     }
 
     public static void writeFully(FileDescriptor fd, ByteBuffer from) throws IOException {
         if (Build.VERSION.SDK_INT >= AndroidVersions.API_23_ANDROID_6_0) {
             while (from.hasRemaining()) {
-                write(fd, from);
+                Api21.write(fd, from);
             }
-        } else {
+        } else if (Build.VERSION.SDK_INT >= AndroidVersions.API_21_ANDROID_5_0) {
             // ByteBuffer position is not updated as expected by Os.write() on old Android versions, so
             // handle the position and the remaining bytes manually.
             // See <https://github.com/Genymobile/scrcpy/issues/291>.
             int position = from.position();
             int remaining = from.remaining();
             while (remaining > 0) {
-                int w = write(fd, from);
+                int w = Api21.write(fd, from);
                 if (BuildConfig.DEBUG && w < 0) {
                     // w should not be negative, since an exception is thrown on error
                     throw new AssertionError("Os.write() returned a negative value (" + w + ")");
@@ -51,6 +62,25 @@ public final class IO {
                 remaining -= w;
                 position += w;
                 from.position(position);
+            }
+        } else {
+            try (FileOutputStream output = new FileOutputStream(fd)) {
+                try (FileChannel channel = output.getChannel()) {
+                    int position = from.position();
+                    int remaining = from.remaining();
+                    while (remaining > 0) {
+                        try {
+                            int w = channel.write(from);
+                            remaining -= w;
+                            position += w;
+                            from.position(position);
+                        } catch (IOException e) {
+                            if (e.getMessage() == null || !e.getMessage().contains("EINTR")) {
+                                throw e;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -69,8 +99,13 @@ public final class IO {
     }
 
     public static boolean isBrokenPipe(IOException e) {
-        Throwable cause = e.getCause();
-        return cause instanceof ErrnoException && ((ErrnoException) cause).errno == OsConstants.EPIPE;
+        if (Build.VERSION.SDK_INT >= AndroidVersions.API_21_ANDROID_5_0) {
+            Throwable cause = e.getCause();
+            return cause instanceof ErrnoException && ((ErrnoException) cause).errno == OsConstants.EPIPE;
+        } else {
+            String message = e.getMessage();
+            return message != null && message.contains("EPIPE");
+        }
     }
 
     public static boolean isBrokenPipe(Exception e) {
