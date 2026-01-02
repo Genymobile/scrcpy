0a3ef93a8dc293a5e3acef688969a0dcf46f448f
Cabbache <cabbache@cabbache.net>
2026-01-02 18:57:25 +0100

    update argument name

--- app/src/cli.c
+++ app/src/cli.c
@@ -558,7 +558,7 @@ static const struct sc_option options[] = {
     },
     {
         .longopt_id = OPT_LIST_AUDIO_SOURCES,
-        .longopt = "list-audio-sources",
+        .longopt = "list-client-audio-sources",
         .text = "List available audio input sources on the client computer.",
     },
     {
