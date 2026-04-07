a2f4ab5c7ecfada2bbd0da1c3d89e6a2eaecaaf8
Romain Vimont <rom@rom1v.com>
2026-04-07 20:35:01 +0200

    Remove frame buffer internal mutex
    Let the caller handle locking as needed, allowing them to perform other
    actions under the same lock.

--- app/src/frame_buffer.c
+++ app/src/frame_buffer.c
@@ -19,13 +19,6 @@ sc_frame_buffer_init(struct sc_frame_buffer *fb) {
         return false;
     }
 
-    bool ok = sc_mutex_init(&fb->mutex);
-    if (!ok) {
-        av_frame_free(&fb->pending_frame);
-        av_frame_free(&fb->tmp_frame);
-        return false;
-    }
-
     // there is initially no frame, so consider it has already been consumed
     fb->pending_frame_consumed = true;
 
@@ -34,7 +27,6 @@ sc_frame_buffer_init(struct sc_frame_buffer *fb) {
 
 void
 sc_frame_buffer_destroy(struct sc_frame_buffer *fb) {
-    sc_mutex_destroy(&fb->mutex);
     av_frame_free(&fb->pending_frame);
     av_frame_free(&fb->tmp_frame);
 }
@@ -47,8 +39,12 @@ swap_frames(AVFrame **lhs, AVFrame **rhs) {
 }
 
 bool
-sc_frame_buffer_push(struct sc_frame_buffer *fb, const AVFrame *frame,
-                     bool *previous_frame_skipped) {
+sc_frame_buffer_has_frame(struct sc_frame_buffer *fb) {
+    return !fb->pending_frame_consumed;
+}
+
+bool
+sc_frame_buffer_push(struct sc_frame_buffer *fb, const AVFrame *frame) {
     // Use a temporary frame to preserve pending_frame in case of error.
     // tmp_frame is an empty frame, no need to call av_frame_unref() beforehand.
     int r = av_frame_ref(fb->tmp_frame, frame);
@@ -57,32 +53,21 @@ sc_frame_buffer_push(struct sc_frame_buffer *fb, const AVFrame *frame,
         return false;
     }
 
-    sc_mutex_lock(&fb->mutex);
-
     // Now that av_frame_ref() succeeded, we can replace the previous
     // pending_frame
     swap_frames(&fb->pending_frame, &fb->tmp_frame);
     av_frame_unref(fb->tmp_frame);
 
-    if (previous_frame_skipped) {
-        *previous_frame_skipped = !fb->pending_frame_consumed;
-    }
     fb->pending_frame_consumed = false;
-
-    sc_mutex_unlock(&fb->mutex);
-
     return true;
 }
 
 void
 sc_frame_buffer_consume(struct sc_frame_buffer *fb, AVFrame *dst) {
-    sc_mutex_lock(&fb->mutex);
     assert(!fb->pending_frame_consumed);
     fb->pending_frame_consumed = true;
 
     av_frame_move_ref(dst, fb->pending_frame);
     // av_frame_move_ref() resets its source frame, so no need to call
     // av_frame_unref()
-
-    sc_mutex_unlock(&fb->mutex);
 }
