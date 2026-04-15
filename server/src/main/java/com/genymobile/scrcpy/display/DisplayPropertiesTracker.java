92ae7de2892bb9d7eadfe6c860ab03351b2f13ed
Romain Vimont <rom@rom1v.com>
2026-04-15 19:40:17 +0200

    Extract display properties tracker
    `DisplayMonitor` listens for system display events. Move the
    event-handling logic into a separate class (`DisplayPropertiesTracker`),
    as it will become more complex in the following commit.

---
+++ server/src/main/java/com/genymobile/scrcpy/display/DisplayPropertiesTracker.java
@@ -0,0 +1,43 @@
+package com.genymobile.scrcpy.display;
+
+import com.genymobile.scrcpy.util.Ln;
+
+public final class DisplayPropertiesTracker {
+    private DisplayProperties props;
+
+    public synchronized void setCurrent(DisplayProperties props) {
+        this.props = props;
+    }
+
+    /**
+     * Set the current display properties, and indicate whether the capture must be reset.
+     *
+     * @param props the current display properties
+     * @return {@code true} if the capture must be reset
+     */
+    public synchronized boolean onDisplayPropertiesChanged(DisplayProperties props) {
+        DisplayProperties prev = this.props;
+        this.props = props;
+
+        if (props == null) {
+            // The display properties have changed, but are unknown, a resize event must be triggered
+            if (Ln.isEnabled(Ln.Level.VERBOSE)) {
+                Ln.v(getClass().getSimpleName() + ": " + prev + " -> (unknown)");
+            }
+            return true;
+        }
+
+        if (props.equals(prev)) {
+            if (Ln.isEnabled(Ln.Level.VERBOSE)) {
+                Ln.v(getClass().getSimpleName() + ": " + props + "(unchanged)");
+            }
+            return false;
+        }
+
+        if (Ln.isEnabled(Ln.Level.VERBOSE)) {
+            Ln.v(getClass().getSimpleName() + ": "  + prev + " -> " + props);
+        }
+
+        return true;
+    }
+}
