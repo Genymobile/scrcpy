a45db12df7bc11297a73e8df4e7921e0e54bc176
Romain Vimont <rom@rom1v.com>
2026-04-24 19:32:44 +0200

    Never increase dimensions when applying alignment
    Previously, the minor dimension was rounded to the nearest multiple of
    the alignment requirement when constraining size. This could result in
    the dimension being increased.
    
    Change the behavior to always round down instead, ensuring the
    constrained size never grows.
    
    This fixes a conflict with the client-side "optimal window size"
    computation, which never increases dimensions. With flex displays, the
    old behavior could lead to feedback loop between window and display
    resizing with mismatched dimensions:
    
     - window  resized to 2341x1317
     - display resized to 2340x1318
     - window  resized to 2338x1317
     - display resized to 2336x1318

--- server/src/test/java/com/genymobile/scrcpy/model/SizeTest.java
+++ server/src/test/java/com/genymobile/scrcpy/model/SizeTest.java
@@ -25,15 +25,15 @@ public class SizeTest {
         Size size = new Size(207, 209);
 
         Assert.assertEquals(size, size.constrain(createVideoConstraints(0, 1)));
-        Assert.assertEquals(new Size(208, 210), size.constrain(createVideoConstraints(0, 2)));
-        Assert.assertEquals(new Size(208, 208), size.constrain(createVideoConstraints(0, 4)));
+        Assert.assertEquals(new Size(206, 208), size.constrain(createVideoConstraints(0, 2)));
+        Assert.assertEquals(new Size(204, 208), size.constrain(createVideoConstraints(0, 4)));
 
         Size s = size.constrain(createVideoConstraints(208, 2));
         Assert.assertEquals(208, s.getHeight());
         Assert.assertTrue(s.getWidth() >= 206 && s.getWidth() <= 208 && s.getWidth() % 2 == 0);
 
         Assert.assertEquals(new Size(207 * 208 / 209, 208), size.constrain(createVideoConstraints(208, 2)));
-        Assert.assertEquals(new Size(208, 208), size.constrain(createVideoConstraints(208, 4)));
+        Assert.assertEquals(new Size(204, 208), size.constrain(createVideoConstraints(208, 4)));
     }
 
     @Test
