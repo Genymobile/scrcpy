Genymobile/scrcpy

commit d3ffab9d7489af6cc0533adeb43e3cfa5b128c81

Add hacky workaround to fix Meta Quest flickering
Scrcpy captures the screen first using the `DisplayManager` API, and if
that fails, it falls back to the `SurfaceControl` API.

On the Meta Quest 3, `DisplayManager` initialization throws a
`RuntimeException` with the message:

    createVirtualDisplay failed. See logcat for error

After this, scrcpy falls back to the `SurfaceControl` API, which starts
mirroring the screen.

The problem is that although `createVirtualDisplay()` throws a
`RuntimeException`, a mirroring session was started anyway.
Consequently, the fallback via `SurfaceControl` starts a second
mirroring session to the same Surface, causing flickering.

On Meta Quest, assume that the mirroring started successfully and create
a dummy virtual display instance that wraps the surface.

Refs #5913 comment <https://github.com/Genymobile/scrcpy/issues/5913#issuecomment-3677889916>
Fixes #5913 <https://github.com/Genymobile/scrcpy/issues/5913>

server/src/main/java/com/genymobile/scrcpy/video/ScreenCapture.java
@@ -133,17 +133,26 @@ public class ScreenCapture extends SurfaceCapture {
                     .createVirtualDisplay("scrcpy", inputSize.getWidth(), inputSize.getHeight(), displayId, surface);
             Ln.d("Display: using DisplayManager API");
         } catch (Exception displayManagerException) {
-            try {
-                display = createDisplay();
+            if (Build.BRAND.equalsIgnoreCase("oculus") && Build.MODEL.toLowerCase().startsWith("quest")) {
+                // Workaround for buggy createVirtualDisplay on Quest
+                try {
+                    virtualDisplay = (VirtualDisplay) VirtualDisplay.class.getDeclaredConstructors()[0].newInstance(null, null, null, surface);
+                } catch (ReflectiveOperationException e) {
+                    Ln.e("Could not create VirtualDisplay", e);
+                }
+            } else {
+                try {
+                    display = createDisplay();
 
-                Size deviceSize = displayInfo.getSize();
-                int layerStack = displayInfo.getLayerStack();
-                setDisplaySurface(display, surface, deviceSize.toRect(), inputSize.toRect(), layerStack);
-                Ln.d("Display: using SurfaceControl API");
-            } catch (Exception surfaceControlException) {
-                Ln.e("Could not create display using DisplayManager", displayManagerException);
-                Ln.e("Could not create display using SurfaceControl", surfaceControlException);
-                throw new AssertionError("Could not create display");
+                    Size deviceSize = displayInfo.getSize();
+                    int layerStack = displayInfo.getLayerStack();
+                    setDisplaySurface(display, surface, deviceSize.toRect(), inputSize.toRect(), layerStack);
+                    Ln.d("Display: using SurfaceControl API");
+                } catch (Exception surfaceControlException) {
+                    Ln.e("Could not create display using DisplayManager", displayManagerException);
+                    Ln.e("Could not create display using SurfaceControl", surfaceControlException);
+                    throw new AssertionError("Could not create display");
+                }
             }
         }
 

commit 247f93e81e2497c33f5ef487a7eceda26db83779

Fix display capture regression on original Oculus Quest
The workaround that @rom1v implemented in d3ffab9d7489af6cc0533adeb43e3cfa5b128c81 broke
the original quest as it has not received updates from meta in multiple years.

This commit restricts the workaround so it only applies to newer device
generations (e.g. the Quest Pro, Quest 2, Quest 3/s) by excluding legacy hardware
whose model identifier lacks a numerical suffix.

server/src/main/java/com/genymobile/scrcpy/video/ScreenCapture.java
@@ -134,8 +134,9 @@ public class ScreenCapture extends SurfaceCapture {
                     .createVirtualDisplay("scrcpy", inputSize.getWidth(), inputSize.getHeight(), displayId, surface);
             Ln.d("Display: using DisplayManager API");
         } catch (Exception displayManagerException) {
-            if (Build.BRAND.equalsIgnoreCase("oculus") && Build.MODEL.toLowerCase(Locale.ROOT).startsWith("quest")) {
+            if (Build.BRAND.equalsIgnoreCase("oculus") && Build.MODEL.startsWith("quest") && !Build.MODEL.equalsIgnoreCase("quest")) {
                 // Workaround for buggy createVirtualDisplay on Quest
+                // Extra workaround to fix the original Quest
                 try {
                     virtualDisplay = (VirtualDisplay) VirtualDisplay.class.getDeclaredConstructors()[0].newInstance(null, null, null, surface);
                 } catch (ReflectiveOperationException e) {