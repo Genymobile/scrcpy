e37ef1a3f4400919c794562c60ebea12e1cc6ba3
doyle31 <abacubabacus@gmail.com>
2026-08-11 00:18:59 +1000

    add the fix @rom1v mentioned

--- server/src/main/java/com/genymobile/scrcpy/video/ScreenCapture.java
+++ server/src/main/java/com/genymobile/scrcpy/video/ScreenCapture.java
@@ -134,7 +134,7 @@ public class ScreenCapture extends SurfaceCapture {
                     .createVirtualDisplay("scrcpy", inputSize.getWidth(), inputSize.getHeight(), displayId, surface);
             Ln.d("Display: using DisplayManager API");
         } catch (Exception displayManagerException) {
-            if (Build.BRAND.equalsIgnoreCase("oculus") && Build.MODEL.startsWith("quest") && !Build.MODEL.equalsIgnoreCase("quest")) {
+            if (Build.BRAND.equalsIgnoreCase("oculus") && Build.MODEL.toLowerCase(Locale.ROOT).startsWith("quest ")) {
                 // Workaround for buggy createVirtualDisplay on Quest
                 // Extra workaround to fix the original Quest
                 try {
